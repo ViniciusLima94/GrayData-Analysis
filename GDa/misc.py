45eed29b44a53f99573872e9f8a488516e094b5f
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2020-04-22 11:25:13 -0300

    added misc.py

---
+++ GDa/misc.py
@@ -0,0 +1,19 @@
+import numpy         as np 
+
+
+class set_paths:
+
+	def __init__(self, raw_path = 'GrayLab/', monkey = 'lucy', date = '150128'):
+		self.date     = date
+		self.monkey   = monkey
+		self.raw_path = raw_path
+
+	def define_paths(self,):
+		self.dir     = self.raw_path + self.monkey + '/' + self.date + '/' + self.session + '/' 
+		self.dir_out = 'Results/'    + self.monkey + '/' + self.date + '/' + self.session + '/' 
+
+		# Create out folder in case it not exist yet
+		try:
+		    os.makedirs(self.dir_out)
+		except:
+		    None
