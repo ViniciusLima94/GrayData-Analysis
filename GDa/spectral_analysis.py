688a02aab5586df6f37d26f3e9428dd96148a222
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2020-04-23 11:28:36 -0300

    updating

--- GDa/spectral_analysis.py
+++ GDa/spectral_analysis.py
@@ -5,7 +5,44 @@ import neo
 import elephant
 from   quantities    import s, Hz
 
-class spectral_analysis():
+
+class spectral():
+
+	def __init__(self, ):
+		None
+
+	def compute_freq(self, N, fs):
+		# Time length
+		T = N / fs
+		# Frequency array
+		f = np.linspace(1/T, fs/2-1/T, N/2+1)
+
+		return f
+
+	def filter(self, signal = None, fs = 20, f_low = 30, f_high = 60, n_jobs = 1):
+		signal_filtered = mne.filter.filter_data(signal, fs, f_low, f_high, 
+												 method = 'iir', verbose=False, n_jobs=n_jobs)
+
+		return signal_filtered
+
+	def wavelet_morlet(self, signal = None, fs = 20):
+
+		N = self.signal.shape[0]
+		f = self.compute_freq(N, fs)
+
+		if len(f) <= 100:
+			f = f
+		else:
+			delta = int( np.ceil(len(f) / 100) )
+			f = f[::delta]
+
+		X = neo.AnalogSignal(signal, t_start = 0*s, sampling_rate = fs*Hz, units='dimensionless')
+		W = elephant.signal_processing.wavelet_transform(X, f, fs=self.fsample).reshape((N,len(f)))
+
+		return W
+
+
+class spectral_analysis(spectral):
 
 	def __init__(self, LFP = None, path = None, step = 25, dt = 250, fc = np.arange(6, 62, 2), df = 4, 
 				save_filtered = False, save_morlet = False, save_coh = True):
@@ -35,18 +72,9 @@ class spectral_analysis():
 			self.data    = LFP.data
 
 		self.results = {}	
-			
-	def compute_freq(self, ):
-		# Time length
-		N  = self.tarray.shape[0]
-		T = N / self.fsample
-		# Frequency array
-		f = np.linspace(1/T, self.fsample/2-1/T, N/2+1)
 
-		return f
-
-	def filter(self, signal = None, trial = None, index_channel = None, f_low = 30, f_high = 60, n_jobs = 1):
-
+	def filter(self, trial = None, index_channel = None, f_low = 30, f_high = 60, n_jobs = 1):
+		'''
 		if trial == None and index_channel == None:
 			signal_filtered = mne.filter.filter_data(signal, self.fsample, f_low, f_high, 
 												 method = 'iir', verbose=False, n_jobs=n_jobs)
@@ -59,9 +87,11 @@ class spectral_analysis():
 			self.results['sigf' + str(f_low) + '_' + str(f_high)] = signal_filtered
 
 		return signal_filtered
+		'''
+		return super(spectral_analysis, self).filter(signal = self.data[trial, index_channel, :], fs = self.fsample, f_low = f_low, f_high = f_high, n_jobs = n_jobs)
 
 	def wavelet_morlet(self, signal = None, trial = None, index_channel = None):
-
+		'''
 		N = self.tarray.shape[0]
 		f = self.compute_freq()
 
@@ -82,21 +112,23 @@ class spectral_analysis():
 			self.results['morlet'] = W
 
 		return W
+		'''
+		return super(spectral_analysis, self).wavelet_morlet(signal = self.data[trial, index_channel, :], fs = self.fsample)
 
 	def pairwise_coherence(self, trial, index_pair, n_jobs = 1):
 			print('Trial: '+str(trial)+', Pair: '+str(index_pair))
-			self.coh = np.empty( [len(self.taxs), len(self.fc)] )
+			coh = np.empty( [len(self.taxs), len(self.fc)] )
 			# First LFP
-			sig1 = self.data[trial, self.pairs[index_pair, 0], :].copy()
+			#sig1 = self.data[trial, self.pairs[index_pair, 0], :].copy()
 			# Second LFP
-			sig2 = self.data[trial, self.pairs[index_pair, 1], :].copy()
+			#sig2 = self.data[trial, self.pairs[index_pair, 1], :].copy()
 
 			S = (1+1j)*np.zeros([3, self.data.shape[2]])
 			for nf in range( self.fc.shape[0] ):
 				bpfreq = np.array( [ self.fc[nf]-self.df, self.fc[nf]+self.df ] )
 				f_low, f_high  = bpfreq[0], bpfreq[1]
-				sig1f  = self.filter(signal = sig1, f_low = f_low, f_high = f_high, n_jobs = n_jobs)
-				sig2f  = self.filter(signal = sig2, f_low = f_low, f_high = f_high, n_jobs = n_jobs)
+				sig1f  = self.filter(trial = trial, index_channel = self.pairs[index_pair, 0], f_low = f_low, f_high = f_high, n_jobs = n_jobs)
+				sig2f  = self.filter(trial = trial, index_channel = self.pairs[index_pair, 1], f_low = f_low, f_high = f_high, n_jobs = n_jobs)
 				Sx     = sig.hilbert(sig1f)
 				Sy     = sig.hilbert(sig2f)
 				S[0, :] = np.multiply( Sx, np.conj(Sy) )
@@ -104,7 +136,7 @@ class spectral_analysis():
 				S[2, :] = np.multiply( Sy, np.conj(Sy) )
 				Sm = sig.convolve2d(S.T, np.ones([self.dt, 1]), mode='same') 
 				Sm = Sm[self.tidx,:]
-				self.coh[:, nf] = ( Sm[:, 0]*np.conj(Sm[:, 0]) /(Sm[:, 1]*Sm[:,2]) ).real
+				coh[:, nf] = ( Sm[:, 0]*np.conj(Sm[:, 0]) /(Sm[:, 1]*Sm[:,2]) ).real
 
 			if save_coh == True:
 				self.results['coherence'] = coh
