64a20bde678fb5640c77fb0a89daa4166b6e072a
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2020-04-23 18:53:01 -0300

    added function to compute coherence in parallel in spectral_analysis

--- run.sh
+++ run.sh
@@ -3,9 +3,8 @@
 #SBATCH -J COH                # Job name
 #SBATCH -o coh.out            # Name of stdout output file (%j expands to %jobID)
 #SBATCH -t 700:00:00          # Run time (hh:mm:ss) - 1.5 hours
-#SBATCH -N 1 
-##SBATCH --exclude=c[01-04],clusterneuromat
-#SBATCH --array=1,2,3,4,5
-
+#SBATCH -n 40
+#SBATCH -N 1
+#SBATCH --array=1:5
 
 python3 save_coherences.py $SLURM_ARRAY_TASK_ID
--- save_coherences.py
+++ save_coherences.py
@@ -1,7 +1,6 @@
 import sys
 import numpy                         as     np
-from   GDa.spectral_analysis         import spectral_analysis 
-#from   GDa.spectral                  import *
+from   GDa.spectral_analysis         import spectral_analysis
 from   joblib import Parallel, delayed
 import multiprocessing
 
@@ -19,35 +18,10 @@ dirs = {'rawdata':'GrayLab/',
         'session':'session01',
         'date'   :[['141014', '141015', '141205', '150128', '150211', '150304'], []]
         }
+        
 path = 'raw_lfp/'+dirs['monkey'][nmonkey]+'_'+'session01'+'_'+dirs['date'][nmonkey][idx]+'.npy'
-'''
-step = 25 
-dt   = 250
-fc   = np.arange(6, 62, 2)
-df   = 4
 
-
-LFP  = np.load(path, allow_pickle=True).item()
-
-nP      = LFP['info']['nP']
-nT      = LFP['info']['nT']
-pairs   = LFP['info']['pairs']
-tarray  = LFP['info']['tarray']
-tidx    = np.arange(dt, LFP['data'].shape[2]-dt, step)
-taxs    = LFP['info']['tarray'][tidx]
-fsample = LFP['info']['fsample']
-data    = LFP['data']
-dir_out = LFP['path']['dir_out']
-
-for trial in range(nT):
-	Parallel(n_jobs=40, backend='loky', max_nbytes=1e6)( delayed(pairwise_coherence)
-						        (data, trial, index_pair, taxs, tidx, fc, df, dt, fsample, pairs, dir_out, n_jobs = 1, save_to_file = True) 
-						        for index_pair in range(nP) 
-						)
-'''
 spectral = spectral_analysis(path = path, step = 25, dt = 250, fc = np.arange(6, 62, 2), df = 4, 
 							 save_filtered = False, save_morlet = False, save_coh = True)
 
 spectral.compute_coherences(n_jobs = -1)
-#for trial in range(spectral.nT):
-#	Parallel(n_jobs=40, backend='threading')( delayed(spectral.pairwise_coherence)(trial, ip, n_jobs = 1, save_to_file = True) for ip in range(spectral.nP) )
