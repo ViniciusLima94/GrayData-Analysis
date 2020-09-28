b5b817747d51e292c5bf0d241aa51129964ff7a6
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2020-09-28 09:18:09 -0300

    plot_data created

--- GDa/plot_data.py
+++ GDa/plot_data.py
@@ -4,10 +4,22 @@
 import numpy             as np
 import matplotlib.pyplot as plt
 
-class plot_data():
+def plot_lfp_samples(data, trials, channels, colors = None):
 
-    def __init__():
-        None
+	count    = 1
+	plt.figure(figsize=(7,4))
+	for i in range( len(trials) ):
+		for j in range( len(channels) ):
+			plt.subplot(len(trials), len(channels), count)
+			plt.subplots_adjust(wspace=.1, hspace=.1)
+			plt.box(False)
+			plt.plot(data[trials[i],channels[j],1000:2000], c = colors[j])
+			plt.xticks([])
+			plt.yticks([])
+			if colors[j] == colors[0]:
+				plt.ylabel('Trial ' + str(trials[i]))
+			if trials[i] == trials[0]:
+				plt.title('Ch. ' + str(channels[j]))
+			count += 1
+	plt.show()
 
-    def plot_sample_lfps():
-        None
