75b0181245e675f344ee4b9eac5f23dfeb6c7b91
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2020-09-29 06:11:18 -0300

    removing checkpoints

--- GDa/spectral_analysis.py
+++ GDa/spectral_analysis.py
@@ -24,7 +24,7 @@ class spectral():
 
 	def filter(self, signal = None, fs = 20, f_low = 30, f_high = 60, n_jobs = 1):
 		signal_filtered = mne.filter.filter_data(signal, fs, f_low, f_high,
-												 method = 'iir', verbose=False, n_jobs=n_jobs)
+							 method = 'iir', verbose=False, n_jobs=n_jobs)
 
 		return signal_filtered
 
