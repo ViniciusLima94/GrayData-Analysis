36b50215899b846b627948e838d7c89949cd423c
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2020-09-30 14:03:41 -0300

    updates on spectral analysis

--- GDa/spectral_analysis.py
+++ GDa/spectral_analysis.py
@@ -4,8 +4,7 @@
 import numpy         as np
 import scipy.signal  as sig
 import mne.filter
-import neo
-import elephant
+import os
 from   quantities    import s, Hz
 from   joblib        import Parallel, delayed
 import multiprocessing
@@ -39,16 +38,12 @@ class spectral():
 
 class spectral_analysis(spectral):
 
-	def __init__(self, session = None, path = None, step = 25, dt = 250, fc = np.arange(6, 62, 2), df = 4,
-				save_filtered = False, save_morlet = False, save_coh = True):
+	def __init__(self, session = None, path = None,):
 
-		self.step    = step
-		self.dt      = dt
-		self.fc      = fc
-		self.df      = df
-		self.save_filtered = save_filtered
-		self.save_morlet   = save_morlet
-		self.save_coh      = save_coh
+
+		#self.save_filtered = save_filtered
+		#self.save_morlet   = save_morlet
+		#self.save_coh      = save_coh
 
 		if session == None:
 			session = np.load(path, allow_pickle=True).item()
@@ -56,8 +51,8 @@ class spectral_analysis(spectral):
 			self.nT      = session['info']['nT']
 			self.pairs   = session['info']['pairs']
 			self.tarray  = session['info']['tarray']
-			self.tidx    = np.arange(self.dt, session['data'].shape[2]-self.dt, self.step)
-			self.taxs    = session['info']['tarray'][self.tidx]
+			#self.tidx    = np.arange(self.dt, session['data'].shape[2]-self.dt, self.step)
+			#self.taxs    = session['info']['tarray'][self.tidx]
 			self.fsample = session['info']['fsample']
 			self.data    = session['data']
 			self.dir     = session['path']['dir']
@@ -67,15 +62,15 @@ class spectral_analysis(spectral):
 			self.nT      = session.nT
 			self.pairs   = session.pairs
 			self.tarray  = session.time[0]
-			self.tidx    = np.arange(self.dt, session.data.shape[2]-self.dt, self.step)
-			self.taxs    = session.time[0][self.tidx]
+			#self.tidx    = np.arange(self.dt, session.data.shape[2]-self.dt, self.step)
+			#self.taxs    = session.time[0][self.tidx]
 			self.fsample = session.recording_info['fsample']
 			self.data    = session.data
 			self.dir     = session.dir
 			self.dir_out = session.dir_out
 
-		self.results = {}
-		self.results['coherence'] = {}
+		#self.results = {}
+		#self.results['coherence'] = {}
 
 	def filter(self, trial = None, index_channel = None, apply_to_all = False, f_low = 30, f_high = 60, n_jobs = 1):
 
@@ -107,35 +102,99 @@ class spectral_analysis(spectral):
 
 		return W
 
-	def instantaneous_power(self, ):
-		None
+	def instantaneous_power(self, trial = None, index_channel = None, f_low = 30, f_high = 60, n_jobs = 1):
+		
+		signal_filtered = self.filter(trial = trial, index_channel = index_channel, 
+									  apply_to_all = False, f_low = f_low, f_high = f_high, 
+									  n_jobs = n_jobs)
 
-	def pairwise_coherence(self, trial, index_pair, n_jobs = 1, save_to_file = True):
-			#print('Trial: '+str(trial)+', Pair: '+str(index_pair))
-			coh = np.empty( [len(self.taxs), len(self.fc)] )
+		# Hilbert transform
+		S               = sig.hilbert(signal_filtered)
+		# Power
+		P               = np.multiply( S, np.conj(S) )
+		return P
 
-			S = (1+1j)*np.zeros([3, self.data.shape[2]])
-			for nf in range( self.fc.shape[0] ):
-				bpfreq = np.array( [ self.fc[nf]-self.df, self.fc[nf]+self.df ] )
-				f_low, f_high  = bpfreq[0], bpfreq[1]
-				sig1f  = self.filter(trial = trial, index_channel = self.pairs[index_pair, 0], f_low = f_low, f_high = f_high, n_jobs = n_jobs)
-				sig2f  = self.filter(trial = trial, index_channel = self.pairs[index_pair, 1], f_low = f_low, f_high = f_high, n_jobs = n_jobs)
-				Sx     = sig.hilbert(sig1f)
-				Sy     = sig.hilbert(sig2f)
-				S[0, :] = np.multiply( Sx, np.conj(Sy) )
-				S[1, :] = np.multiply( Sx, np.conj(Sx) )
-				S[2, :] = np.multiply( Sy, np.conj(Sy) )
-				Sm = sig.convolve2d(S.T, np.ones([self.dt, 1]), mode='same')
-				Sm = Sm[self.tidx,:]
-				coh[:, nf] = ( Sm[:, 0]*np.conj(Sm[:, 0]) /(Sm[:, 1]*Sm[:,2]) ).real
+	'''
+	def pairwise_coherence(self, trial = None, index_pair = None, step = 25, dt = 250, 
+					       fc = np.arange(6, 62, 2), df = 4, n_jobs = 1, save_to_file = True):
 
-			if self.save_coh == True:
-				self.results['coherence'][str(trial)] = {}
-				self.results['coherence'][str(trial)][str(index_pair)] = coh
+		self.step    = step
+		self.dt      = dt
+		self.fc      = fc
+		self.df      = df
 
-			if save_to_file == True:
-				file_name = self.dir_out + 'trial_' +str(trial) + '_pair_' + str(self.pairs[index_pair, 0]) + '_' + str(self.pairs[index_pair, 1]) + '.npy'
-				np.save(file_name, {'coherence' : coh})
+		#self.tidx    = np.arange(self.dt, session['data'].shape[2]-self.dt, self.step)
+
+		#print('Trial: '+str(trial)+', Pair: '+str(index_pair))
+		#coh = np.empty( [len(self.taxs), len(self.fc)] )
+		coh = np.empty( [len(self.tarray), len(self.fc)] )
+
+		S = (1+1j)*np.zeros([3, self.data.shape[2]])
+		for nf in range( self.fc.shape[0] ):
+			bpfreq = np.array( [ self.fc[nf]-self.df, self.fc[nf]+self.df ] )
+			f_low, f_high  = bpfreq[0], bpfreq[1]
+			sig1f  = self.filter(trial = trial, index_channel = self.pairs[index_pair, 0], 
+								 f_low = f_low, f_high = f_high, n_jobs = n_jobs)
+			sig2f  = self.filter(trial = trial, index_channel = self.pairs[index_pair, 1], 
+				                 f_low = f_low, f_high = f_high, n_jobs = n_jobs)
+			Sx     = sig.hilbert(sig1f)
+			Sy     = sig.hilbert(sig2f)
+			S[0, :] = np.multiply( Sx, np.conj(Sy) )
+			S[1, :] = np.multiply( Sx, np.conj(Sx) )
+			S[2, :] = np.multiply( Sy, np.conj(Sy) )
+			Sm = sig.convolve2d(S.T, np.ones([self.dt, 1]), mode='same')
+			#Sm = Sm[self.tidx,:]
+			coh[:, nf] = ( Sm[:, 0]*np.conj(Sm[:, 0]) /(Sm[:, 1]*Sm[:,2]) ).real
+
+		#if self.save_coh == True:
+		#	self.results['coherence'][str(trial)] = {}
+	#		self.results['coherence'][str(trial)][str(index_pair)] = coh
+
+		if save_to_file == True:
+			file_name = self.dir_out + 'trial_' +str(trial) + '_pair_' + str(self.pairs[index_pair, 0]) + '_' + str(self.pairs[index_pair, 1]) + '.npy'
+			np.save(file_name, {'coherence' : coh})
+
+		else:
+			return coh
+		'''
+
+	def pairwise_coherence(self, trial = None, index_pair = None, dt = 250, 
+				       	   fc = np.arange(6, 62, 2), df = 4, n_jobs = 1, save_to_file = True):
+
+		self.dt      = dt
+		self.fc      = fc
+		self.df      = df
+
+		# Coherence matrix
+		coh = np.empty( [len(self.tarray), len(self.fc)] )
+		# Instantaneous power spectrum matrices
+		S = (1+1j)*np.zeros([3, self.data.shape[2]])
+		# Computing coherences
+		for i in range( self.fc.shape[0] ):
+			# Low and high frequency
+			f_low, f_high = self.fc[i]-self.df, self.fc[i]+self.df
+			# Channel 1 and 2 indexis
+			ch1, ch2 = self.pairs[index_pair, 0], self.pairs[index_pair, 1]
+			# Filtering each signal
+			sig1f  = self.filter(trial = trial, index_channel = ch1, 
+							 f_low = f_low, f_high = f_high, n_jobs = n_jobs)
+			sig2f  = self.filter(trial = trial, index_channel = ch2, 
+			                 f_low = f_low, f_high = f_high, n_jobs = n_jobs)
+			Sx     = sig.hilbert(sig1f)
+			Sy     = sig.hilbert(sig2f)
+			# Computing instantaneous power spectrum
+			S[0, :] = np.multiply( Sx, np.conj(Sy) )
+			S[1, :] = np.multiply( Sx, np.conj(Sx) )
+			S[2, :] = np.multiply( Sy, np.conj(Sy) )
+			Sm = sig.convolve2d(S.T, np.ones([self.dt, 1]), mode='same')
+			coh[:, i] = ( Sm[:, 0]*np.conj(Sm[:, 0]) / (Sm[:, 1]*Sm[:,2]) ).real
+
+		if save_to_file == True:
+			file_name = os.path.join( self.dir_out[:-1], 
+				'trial_' +str(trial) + '_pair_' + str(self.pairs[index_pair, 0]) + '_' + str(self.pairs[index_pair, 1]) + '.npy')
+			np.save(file_name, {'coherence' : coh})
+		else:
+			return coh
 
 	def compute_coherences(self, n_jobs = 1):
 		for trial in range(self.nT):
