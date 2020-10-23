f507edf0518d1ee1af4c4e08f5075bacd144d600
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2020-10-23 11:39:32 -0300

    improving coherence computation

--- GDa/misc/smooth_spectra.py
+++ GDa/misc/smooth_spectra.py
@@ -1,9 +1,9 @@
 import scipy.signal
 import numpy as np
 
-def smooth_spectra(spectra, win_time, win_freq, fft=False):
-	kernel = np.ones([win_time, win_freq])	
-	if fft == True:
-		return scipy.signal.fftconvolve(spectra, kernel, mode='same')
-	else:
-		return scipy.signal.convolve2d(spectra, kernel, mode='same')
+def smooth_spectra(spectra, win_time, win_freq, fft=False, axes = 0):
+    kernel = np.ones([1, win_freq, win_time])
+    if fft == True:
+        return scipy.signal.fftconvolve(spectra, kernel, mode='same', axes= axes)
+    else:
+        return scipy.signal.convolve2d( spectra, kernel, mode='same', axes = axes) 
