8c653f9502cdaed91230a732ba271d8545e2f816
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2020-11-02 15:42:12 -0300

    bug on gabor solved, network class created

--- GDa/misc/smooth_spectra.py
+++ GDa/misc/smooth_spectra.py
@@ -2,8 +2,14 @@ import scipy.signal
 import numpy as np
 
 def smooth_spectra(spectra, win_time, win_freq, fft=False, axes = 0):
-    kernel = np.ones([1, win_freq, win_time])
-    if fft == True:
-        return scipy.signal.fftconvolve(spectra, kernel, mode='same', axes= axes)
-    else:
-        return scipy.signal.convolve2d( spectra, kernel, mode='same', axes = axes) 
+	if len(spectra.shape) == 2:
+		kernel = np.ones([win_freq, win_time])
+	elif len(spectra.shape) == 3:
+		kernel = np.ones([1, win_freq, win_time])
+
+	print(kernel.shape)
+
+	if fft == True:
+		return scipy.signal.fftconvolve(spectra, kernel, mode='same', axes= axes)
+	else:
+		return scipy.signal.convolve2d( spectra, kernel, mode='same', axes = axes) 
