095bc0aa66d1154d4820b8818769c04eb7ef90da
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2020-11-08 12:24:25 -0300

    functions to plot brain sketch

---
+++ GDa/graphics/plot_brain_sketch.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt 
+import numpy             as np
+import scipy.ndimage 
+import scipy.io 
+
+xy                = scipy.io.loadmat('Brain Areas/lucy_brainsketch_xy.mat')['xy'] # Channels coordinates
+ethyl_brainsketch = scipy.ndimage.imread('Brain Areas/ethyl_brainsketch.jpg')     # Brainsketch 
+
+def plot_node_brain_sketch(node_list, node_size):
+    plt.imshow(ethyl_brainsketch)
+    for i,c in zip(range(len(node_list)), node_list):
+        plt.plot(xy[c-1,0], xy[c-1,1], 'mo', ms = node_size[i])
+    plt.xticks([])
+    plt.yticks([])
+
+def plot_edge_brain_sketch(edge_list, node_list, edge_width):
+    plt.imshow(ethyl_brainsketch)
+    for i in range(edge_list.shape[0]):
+        c1, c2 = node_list[edge_list[i,0]], node_list[edge_list[i,1]]
+        p1  = [xy[c1-1,0], xy[c2-1,0]]
+        p2  = [xy[c1-1,1], xy[c2-1,1]]
+        plt.plot(p1, p2, 'b.-', lw = edge_width[i])
+    plt.xticks([])
+    plt.yticks([])
---
+++ GDa/temporal_network.py
@@ -0,0 +1,59 @@
+import numpy            as     np
+import networkx         as     nx
+import os
+import h5py
+import multiprocessing
+from   joblib           import Parallel, delayed
+
+
+class temporal_network():
+
+    def __init__(self, monkey='lucy', date=150128, session=1):
+        # Setting up mokey and recording info to load and save files
+        self.monkey   = monkey
+        self.date     = date                            
+        self.session  = 'session0' + str(session)       
+        
+        # Path to the session information in npy format
+        npy_raw_lfp_path     = os.path.join('raw_lfp', monkey+'_'+'session0'+str(session)+'_'+str(date)+'.npy')
+        # Path to the super tensor in h5 format 
+        h5_super_tensor_path = os.path.join('super_tensors', monkey+'_'+'session0'+str(session)+'_'+str(date)+'.h5')
+        # Loading session info
+        self.session_info = np.load(npy_raw_lfp_path, allow_pickle=True).item()['info']
+        # Loading super teson (temporal network)
+        with h5py.File(h5_super_tensor_path, 'r') as hf:
+            #  print(list(hf.keys()))
+            self.super_tensor = np.array( hf.get('supertensor') )
+            self.tarray       = np.array( hf.get('tarray') )
+            self.freqs        = np.array( hf.get('freqs') )
+            self.bands        = np.array( hf.get('bands') )
+            
+        # Concatenate trials in the super tensor
+        self.super_tensor = self.super_tensor.swapaxes(1,2)
+        self.super_tensor = self.super_tensor.reshape( (self.session_info['nP'], len(self.bands), self.session_info['nT']*len(self.tarray) ) )
+
+    def convert_to_adjacency(self,):
+        self.A = np.zeros([self.session_info['nC'], self.session_info['nC'], len(self.bands), self.session_info['nT']*len(self.tarray)]) 
+        for p in range(self.session_info['pairs'].shape[0]):
+            i, j              = self.session_info['pairs'][p,0], self.session_info['pairs'][p,1]
+            self.A[i,j,:,:]   = self.super_tensor[p,:,:]
+
+    def compute_nodes_strength(self,):
+        self.node_degree = self.A.sum(axis=1)
+
+    def compute_nodes_clustering(self,):
+        None
+
+    def instantiate_graph(self, ):
+        None
+
+    def create_stages_time_grid(self, ):
+        t_cue_off  = (self.session_info['t_cue_off']-self.session_info['t_cue_on'])/self.session_info['fsample']
+        t_match_on = (self.session_info['t_match_on']-self.session_info['t_cue_on'])/self.session_info['fsample']
+        tt         = np.tile(self.tarray, (self.session_info['nT'], 1))
+        #  Create grids with starting and ending times of each stage for each trial
+        self.t_baseline = ( (tt<0) ).reshape(self.session_info['nT']*len(self.tarray))
+        self.t_cue      = ( (tt>=0)*(tt<t_cue_off[:,None]) ).reshape(self.session_info['nT']*len(self.tarray))
+        self.t_delay    = ( (tt>=t_cue_off[:,None])*(tt<t_match_on[:,None]) ).reshape(self.session_info['nT']*len(self.tarray))
+        self.t_match    = ( (tt>=t_match_on[:,None]) ).reshape(self.session_info['nT']*len(self.tarray))
+
