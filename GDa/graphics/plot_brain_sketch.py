0aa3fcf4585fd7be0671551259cf72b73c7d49dd
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2020-11-30 13:07:43 -0300

    update on method to plot on brain surface

--- GDa/graphics/plot_brain_sketch.py
+++ GDa/graphics/plot_brain_sketch.py
@@ -2,14 +2,17 @@ import matplotlib.pyplot as plt
 import numpy             as np
 import scipy.ndimage 
 import scipy.io 
+from matplotlib          import cm
 
 xy                = scipy.io.loadmat('Brain Areas/lucy_brainsketch_xy.mat')['xy'] # Channels coordinates
 ethyl_brainsketch = scipy.ndimage.imread('Brain Areas/ethyl_brainsketch.jpg')     # Brainsketch 
 
-def plot_node_brain_sketch(node_list, node_size):
+def plot_node_brain_sketch(node_list, node_size, alpha, beta, cmap):
+    #  Colormap
+    mapf = cm.get_cmap(cmap, 20)
     plt.imshow(ethyl_brainsketch)
     for i,c in zip(range(len(node_list)), node_list):
-        plt.plot(xy[c-1,0], xy[c-1,1], 'mo', ms = node_size[i])
+        plt.plot(xy[c-1,0], xy[c-1,1], 'o', ms = alpha*np.abs(node_size[i])**beta, color = mapf(node_size[i]))
     plt.xticks([])
     plt.yticks([])
 
