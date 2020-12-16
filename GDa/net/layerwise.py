35e8330f6cd3239f12e8363d59a8b6479423d797
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2020-12-16 07:12:36 -0300

    update temp. net. and restructuring net. measures code

--- GDa/net/layerwise.py
+++ GDa/net/layerwise.py
@@ -42,92 +42,102 @@ def compute_nodes_clustering(A, thr = None):
 
     return np.nan_to_num( clustering )
 
-def compute_nodes_coreness(self, band = 0, thr = None, use='networkx', on_null = False, randomize='edges'):
-    #  Variable to store coreness
-    coreness    = np.zeros([self.session_info['nC'], self.super_tensor.shape[2]])
+def compute_nodes_coreness(A, thr=None):
+    # Check the dimension
+    assert len(A.shape)==3, "The adjacency tensor should be 3D."
+    assert thr != None, "A threshold value should be provided."
 
-    #  if on_null == True:
-    #      self.create_null_model(band = band, thr = thr, randomize=randomize, seed = seed)
+    #  Number of channels
+    nC = A.shape[0]
+    #  Number of observations
+    nt = A.shape[-1]
+    #  Variable to store node coreness
+    coreness  = np.zeros([nC,nt])
 
-    for t in tqdm(range(self.A.shape[3])):
-        #  g = self.instantiate_graph(band,t,thr=thr,on_null=on_null)
-        if on_null == False:
-            g = self.instantiate_graph(self.A[:,:,band,t], thr = thr)
-        else:
-            try:
-                g = self.instantiate_graph(self.A_null[randomize][str(band)][:,:,t], thr = thr)
-            except (AttributeError, KeyError):
-                print('Null model for band ' + str(band) + ' not created yet.')
+    for t in tqdm(range(nt)):
+        g             = instantiate_graph(A[:,:,t], thr=thr)
+        coreness[:,t] = g.coreness()
 
-        if use=='networkx':
-            coreness[:,t] = list( dict( nx.core_number(g) ).values() )
-        elif use=='igraph':
-            coreness[:,t] = ig.Graph(self.session_info['nC'], g.edges).coreness()
     return coreness
 
-def compute_network_partition(self, band = 0, thr = None, use='networkx', on_null=False, randomize='edges'):
+def compute_network_partition(A, thr=None):
+    # Check the dimension
+    assert len(A.shape)==3, "The adjacency tensor should be 3D."
+    assert thr != None, "A threshold value should be provided."
+
+    #  Number of channels
+    nC = A.shape[0]
+    #  Number of observations
+    nt = A.shape[-1]
+    
+    #  Save the partitions
     partition = []
-    #  if on_null == True:
-    #      self.create_null_model(band = band, randomize=randomize, seed = seed)
 
-    for t in tqdm(range(self.A.shape[3])):
-        #  g = self.instantiate_graph(band,t,thr=thr, on_null=on_null)
-        if on_null == False:
-            g = self.instantiate_graph(self.A[:,:,band,t], thr = thr)
-        else:
-            try:
-                g = self.instantiate_graph(self.A_null[randomize][str(band)][:,:,t], thr = thr)
-            except (AttributeError, KeyError):
-                print('Null model for band ' + str(band) + ' not created yet.')
+    for t in tqdm(range(nt)):
+        g = instantiate_graph(A[:,:,t], thr=thr)
+        # Uses leidenalg
+        partition.append(leidenalg.find_partition(g, leidenalg.ModularityVertexPartition))
 
-        if use=='networkx':
-            partition.append(nx.algorithms.community.greedy_modularity_communities(g))
-        elif use=='igraph':
-            #  This one uses leidenalg
-            partition.append(leidenalg.find_partition(ig.Graph(self.session_info['nC'], g.edges), leidenalg.ModularityVertexPartition))
     return partition
 
-def compute_network_modularity(self, band = 0, thr = None, use='networkx', on_null=False, randomize='edges', seed = 0):
+def compute_network_modularity(A, thr=None):
+    # Check the dimension
+    assert len(A.shape)==3, "The adjacency tensor should be 3D."
+    assert thr != None, "A threshold value should be provided."
+
+    #  Number of observations
+    nt = A.shape[-1]
+
     #  Variable to store modularity
-    modularity  = np.zeros(self.super_tensor.shape[2])
-    #  if on_null == True:
-    #      self.create_null_model(band = band, thr = thr, randomize=randomize, seed = seed)
+    modularity  = np.zeros(nt)
 
-    for t in tqdm(range(self.A.shape[3])):
-        #  g = self.instantiate_graph(band,t,thr=thr, on_null=on_null)#nx.Graph(self.A[:,:,band,t]>thr)
-        if on_null == False:
-            g = self.instantiate_graph(self.A[:,:,band,t], thr = thr)
-        else:
-            try:
-                g = self.instantiate_graph(self.A_null[randomize][str(band)][:,:,t], thr = thr)
-            except (AttributeError, KeyError):
-                print('Null model for band ' + str(band) + ' not created yet.')
+    for t in tqdm(range(nt)):
+        g = instantiate_graph(A[:,:,t], thr=thr)
+        modularity[t] = leidenalg.find_partition(g, leidenalg.ModularityVertexPartition).modularity
 
-        if use=='networkx':
-            comm = nx.algorithms.community.greedy_modularity_communities(g)
-            modularity[t] = nx.algorithms.community.modularity(g,comm)
-        elif use=='igraph':
-            #  This one uses leidenalg
-            modularity[t] = leidenalg.find_partition(ig.Graph(self.session_info['nC'], g.edges), leidenalg.ModularityVertexPartition).modularity
     return modularity
 
-def compute_nodes_betweenness(self, k = None, band = 0, thr = None, use='networkx', on_null=False, randomize='edges', seed = 0):
-    betweenness = np.zeros([self.session_info['nC'], self.super_tensor.shape[2]])
-    #  if on_null == True:
-    #      self.create_null_model(band = band, randomize=randomize, seed = seed)
+def compute_nodes_betweenness(A, thr=None):
+    # Check the dimension
+    assert len(A.shape)==3, "The adjacency tensor should be 3D."
 
-    for t in tqdm(range(self.A.shape[3])):
-        #  g = self.instantiate_graph(band,t,thr=thr, on_null=on_null)
-        if on_null == False:
-            g = self.instantiate_graph(self.A[:,:,band,t], thr = thr)
+    #  Number of channels
+    nC = A.shape[0]
+    #  Number of observations
+    nt = A.shape[-1]
+
+    betweenness = np.zeros([nC, nt])
+
+    for t in tqdm(range(nt)):
+        g                = instantiate_graph(A[:,:,t], thr=thr)
+        if thr is None:
+            betweenness[:,t] = g.betweenness(weights="weight")
         else:
-            try:
-                g = self.instantiate_graph(self.A_null[randomize][str(band)][:,:,t], thr = thr)
-            except (AttributeError, KeyError):
-                print('Null model for band ' + str(band) + ' not created yet.')
+            betweenness[:,t] = g.betweenness()
 
-        if use=='networkx':
-            betweenness[:,t] = list( dict( nx.betweenness_centrality(g, k) ).values() )
-        elif use=='igraph':
-            betweenness[:,t] = ig.Graph(self.session_info['nC'], g.edges).betweenness()
     return betweenness
+
+def compute_allegiance_matrix(A, thr=None):
+    # Check the dimension
+    assert len(A.shape)==3, "The adjacency tensor should be 3D."
+    assert thr != None, "A threshold value should be provided."
+
+    #  Number of channels
+    nC = A.shape[0]
+    #  Number of observations
+    nt = A.shape[-1]
+
+    #  Find the partitions
+    p = compute_network_partition(A, thr = thr)
+
+    T = np.zeros([nC, nC])
+    for i in tqdm( range(len(p)) ):
+        n_comm = len(p[i])
+        for j in range(n_comm):
+            grid = np.meshgrid(list(p[i][j]), list(p[i][j]))
+            grid = np.reshape(grid, (2, len(list(p[i][j]))**2)).T
+            T[k, grid[:,0], grid[:,1]] += 1
+    T[k] = T[k] / len(p)
+    np.fill_diagonal(T[k], 0)
+
+    return T
