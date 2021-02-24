0a6bc9030127c4bbb89d3deafe959bfb822e7e61
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2021-02-24 10:56:44 -0300

    update on notebook 5

--- GDa/misc/create_grids.py
+++ GDa/misc/create_grids.py
@@ -1,17 +1,13 @@
 import numpy as np
 
-#  def create_stim_grid(self, ):
-#      #  Number of different stimuli
-#      if not np.isnan(self.session_info['stim']).all():
-#          n_stim           = int((self.session_info['stim']).max()) 
-#          self.stim_grid   = np.zeros([n_stim, self.session_info['nT']*len(self.tarray)])
-#          #  Repeate each stimulus to match the length of the trial 
-#          stim             = np.repeat(self.session_info['stim']-1, len(self.tarray) )
-#          for i in range(n_stim):
-#              self.stim_grid[i] = (stim == i).astype(bool)
-#      else:
-#          self.stim_grid   = np.zeros([n_stim, self.session_info['nT']*len(self.tarray)])
-#          return self.stim_grid.astype(bool)
+def create_stim_grid(stim_list, nT, nt):
+    n_stim           = int(stim_list.max()) 
+    self.stim_grid   = np.zeros([n_stim, nT*nt])
+    #  Repeate each stimulus to match the length of the trial 
+    stim             = np.repeat(stim_list-1, nt )
+    for i in range(n_stim):
+        self.stim_grid[i] = (stim == i).astype(bool)
+    return self.stim_grid.astype(bool)
 
 def create_stages_time_grid(t_cue_on, t_cue_off, t_match_on, fsample, tarray, ntrials):
     t_cue_off  = (t_cue_off-t_cue_on)/fsample
