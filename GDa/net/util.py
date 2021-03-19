14d602ce9414db9642c2d00aa57018f556e46d8c
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2021-03-19 09:35:07 -0300

    update on tmeporal networks, net/util

--- GDa/net/util.py
+++ GDa/net/util.py
@@ -1,4 +1,6 @@
-import igraph as ig
+import igraph as ig 
+import numpy  as np
+from   scipy  import stats
 
 def instantiate_graph(A, thr = None):
     if thr is None:
@@ -7,5 +9,29 @@ def instantiate_graph(A, thr = None):
         g = ig.Graph.Adjacency((A>thr).tolist(), mode=ig.ADJ_UNDIRECTED)
     return g
 
+def compute_thresholds(tensor, q=0.8, relative=False):
+    r'''
+    Compute the power/coherence thresholds for the data
+    > INPUTS:
+    - tensor: Data with dimensions [nodes/links,bands,observations]
+    - q: Quartile value to use as threshold
+    - relative: If True compute one threshold for each node/link in each band (defalta False)
+    > OUTPUTS:
+    - coh_thr: Threshold values, if realtive is True it will have dimensions [bands, nodes/links] otherwise [bands]
+    '''
+    n_nodes, n_bands, n_obs = tensor.shape[0], tensor.shape[1], tensor.shape[2]
+    if relative:
+        coh_thr = np.zeros([n_bands, n_nodes])
+        for i in range( n_bands ):
+            coh_thr[i] = np.squeeze( stats.mstats.mquantiles(tensor[:,i,:], prob=q, axis=1) )
+    else:
+        coh_thr = np.zeros( n_bands )
+        for i in range( n_bands ):
+            coh_thr[i] = stats.mstats.mquantiles(tensor[:,i,:].flatten(), prob=q)
+    return coh_thr
+
 def check_symmetric(a, rtol=1e-05, atol=1e-08):
-        return numpy.allclose(a, a.T, rtol=rtol, atol=atol)
+    r'''
+    Check if the matrix a is symmetric
+    '''
+    return numpy.allclose(a, a.T, rtol=rtol, atol=atol)
--- GDa/temporal_network.py
+++ GDa/temporal_network.py
@@ -1,12 +1,43 @@
-import numpy            as     np
-from   scipy                 import stats
+import numpy                as     np
+from   GDa.misc.reshape     import reshape_trials, reshape_observations
+from   GDa.net.util         import compute_thresholds
+from   scipy                import stats
 import os
 import h5py
 
 class temporal_network():
 
     def __init__(self, raw_path = 'super_tensors', monkey='lucy', session=1, date=150128, align_to = 'cue', 
-                 trial_type = 1, behavioral_response = 1, wt_0 = None, wt_1 = None, trim_borders = False):
+                 trial_type = 1, behavioral_response = 1, wt = (None,None), trim_borders = False,
+                 threshold=False, relative=False, q=0.8):
+        r'''
+        Temporal network class, this object will have information about the session analysed and store the coherence
+        networks (a.k.a. supertensor).
+        > INPUTS:
+        - raw_path: Raw path to the coherence super tensor
+        - monkey: Monkey name
+        - session: session number
+        - date: date of the recording session
+        - align_to: Wheter data is aligned to cue or match
+        - trial_type: the type of trial (DRT/fixation) 
+        - wt: Tuple. Trimming window will remove the wt[0] and wt[1] points in the start and end of each trial
+        - trim_borders: Wheter to trim or not the start/end of the super_tensor for each trial
+        - threshold: Wheter to threshold or not the data
+        - relative: if threshold is true wheter to do it in ralative (one thr per link per band) or an common thr for links per band
+        - q: Quartile value to use for thresholding
+        '''
+
+        #Check for incorrect parameter values
+        if monkey not in ['lucy', 'ethyl']:
+            raise ValueError('monkey should be either "lucy" or "ethyl"')
+        if align_to not in ['cue', 'match']:
+            raise ValueError('align_to should be either "cue" or "match"')
+        if behavioral_response not in [0, 1, None]:
+            raise ValueError('behavioral_response should be either 0 (correct), 1 (incorrect) or None (both)')
+        if trial_type not in [1, 2, 3, 4]:
+            raise ValueError('trial_type should be either 1 (DRT), 2 (intervealed fixation), 3 (blocked fixation) or 4 (blank trials)')       
+        if trial_type in [2,3] and behavioral_response is not None:
+            raise ValueError('For trial type 2 or 3 behavioral_response should be None')
 
         # Setting up mokey and recording info to load and save files
         self.raw_path = raw_path
@@ -21,17 +52,23 @@ class temporal_network():
         self.__load_h5()
 
         if trim_borders == True:
-            self.tarray       = self.tarray[wt_0:-wt_1] 
-            self.super_tensor = self.super_tensor[:,:,:,wt_0:-wt_1]
+            self.tarray       = self.tarray[wt[0]:-wt[1]]
+            self.super_tensor = self.super_tensor[:,:,:,wt[0]:-wt[1]]
 
         # Concatenate trials in the super tensor
         self.super_tensor = self.super_tensor.swapaxes(1,2)
-        self.super_tensor = self.reshape_observations(self.super_tensor)
+        self.super_tensor = self.reshape_observations()
 
         # Will store the null model
         self.A_null = {}
         self.A_null['time']  = {} # Time randomization 
         self.A_null['edges'] = {} # Edges randomization 
+
+        # Threshold the super tensor
+        if thr:
+            coh_thr = compute_thresholds(self.super_tensor, q=q, relative=relative)
+            for i in range( len(self.bands) ):
+                self.super_tensor[:,i,:] = self.super_tensor[:,i,:]>coh_thr[i]
 
     def __load_h5(self,):
         # Path to the super tensor in h5 format 
@@ -43,8 +80,8 @@ class temporal_network():
             raise OSError('File for monkey ' + str(self.monkey) + ', date ' + str(self.date) + ' ' + self.session + ' not created yet')
 
         group = os.path.join('trial_type_'+str(self.trial_type), 
-                         'aligned_to_' + str(self.align_to),
-                         'behavioral_response_'+str(self.behavioral_response)) 
+                             'aligned_to_' + str(self.align_to),
+                             'behavioral_response_'+str(self.behavioral_response)) 
 
         g1 = hf.get(group)
         # Read coherence data
@@ -60,44 +97,46 @@ class temporal_network():
             else:
                 self.session_info[k] = np.squeeze( np.array(g1['info/'+k]) )
 
-    def convert_to_adjacency(self,):
-        self.A = np.zeros([self.session_info['nC'], self.session_info['nC'], len(self.bands), self.session_info['nT']*len(self.tarray)]) 
-        for p in range(self.session_info['pairs'].shape[0]):
-            i, j              = self.session_info['pairs'][p,0], self.session_info['pairs'][p,1]
-            self.A[i,j,:,:]   = self.super_tensor[p,:,:]
+#    def compute_coherence_thresholds(self, q = .80, relative = False):
+#        #  Coherence thresholds
+#        if relative:
+#            self.coh_thr = np.zeros([len(self.bands), self.session_info['pairs'].shape[0]]) 
+#            for i in range(len(self.bands)):
+#                self.coh_thr[i] = np.squeeze( stats.mstats.mquantiles(self.super_tensor[:,i,:], prob=q, axis=1) )
+#        else:
+#            self.coh_thr = np.zeros(len(self.bands)) 
+#            for i in range(len(self.bands)):
+#                self.coh_thr[i] = stats.mstats.mquantiles(self.super_tensor[:,i,:].flatten(), prob=q)
+#
+#    def convert_to_adjacency(self, thr = None):
+#        self.A = np.zeros([self.session_info['nC'], self.session_info['nC'], len(self.bands), self.session_info['nT']*len(self.tarray)]) 
+#        for p in range(self.session_info['pairs'].shape[0]):
+#            i, j              = self.session_info['pairs'][p,0], self.session_info['pairs'][p,1]
+#            self.A[i,j,:,:]   = self.super_tensor[p,:,:]
 
-    def compute_coherence_thresholds(self, q = .80, relative = False):
-        #  Coherence thresholds
-        if relative:
-            self.coh_thr = np.zeros([len(self.bands), self.session_info['pairs'].shape[0]]) 
-            for i in range(len(self.bands)):
-                self.coh_thr[i] = np.squeeze( stats.mstats.mquantiles(self.super_tensor[:,i,:], prob=q, axis=1) )
-        else:
-            self.coh_thr = np.zeros(len(self.bands)) 
-            for i in range(len(self.bands)):
-                self.coh_thr[i] = stats.mstats.mquantiles(self.super_tensor[:,i,:].flatten(), prob=q)
-
-    def reshape_trials(self, tensor):
+    def reshape_trials(self, ):
         #  Reshape the tensor to have trials and time as two separeted dimension
         #  print(len(tensor.shape))
-        if len(tensor.shape) == 1:
-            aux = tensor.reshape([self.session_info['nT'], len(self.tarray)])
-        if len(tensor.shape) == 2:
-            aux = tensor.reshape([tensor.shape[0], self.session_info['nT'], len(self.tarray)])
-        if len(tensor.shape) == 3:
-            aux = tensor.reshape([tensor.shape[0], tensor.shape[1], self.session_info['nT'], len(self.tarray)])
-        if len(tensor.shape) == 4:
-            aux = tensor.reshape([tensor.shape[0], tensor.shape[1], tensor.shape[2], self.session_info['nT'], len(self.tarray)])
+        # if len(tensor.shape) == 1:
+        #     aux = tensor.reshape([self.session_info['nT'], len(self.tarray)])
+        # if len(tensor.shape) == 2:
+        #     aux = tensor.reshape([tensor.shape[0], self.session_info['nT'], len(self.tarray)])
+        # if len(tensor.shape) == 3:
+        #     aux = tensor.reshape([tensor.shape[0], tensor.shape[1], self.session_info['nT'], len(self.tarray)])
+        # if len(tensor.shape) == 4:
+        #     aux = tensor.reshape([tensor.shape[0], tensor.shape[1], tensor.shape[2], self.session_info['nT'], len(self.tarray)])
+        aux = reshape_trials( self.super_tensor, self.session_info['nT'], len(self.tarray) )
         return aux
 
-    def reshape_observations(self, tensor):
+    def reshape_observations(self, ):
         #  Reshape the tensor to have all the trials concatenated in the same dimension
-        if len(tensor.shape) == 2:
-            aux = tensor.reshape([self.session_info['nT'] * len(self.tarray)])
-        if len(tensor.shape) == 3:
-            aux = tensor.reshape([tensor.shape[0], self.session_info['nT'] * len(self.tarray)])
-        if len(tensor.shape) == 4:
-            aux = tensor.reshape([tensor.shape[0], tensor.shape[1], self.session_info['nT'] * len(self.tarray)])
-        if len(tensor.shape) == 5:
-            aux = tensor.reshape([tensor.shape[0], tensor.shape[1], tensor.shape[2], self.session_info['nt'] * len(self.tarray)])
+        # if len(tensor.shape) == 2:
+        #     aux = tensor.reshape([self.session_info['nT'] * len(self.tarray)])
+        # if len(tensor.shape) == 3:
+        #     aux = tensor.reshape([tensor.shape[0], self.session_info['nT'] * len(self.tarray)])
+        # if len(tensor.shape) == 4:
+        #     aux = tensor.reshape([tensor.shape[0], tensor.shape[1], self.session_info['nT'] * len(self.tarray)])
+        # if len(tensor.shape) == 5:
+        #     aux = tensor.reshape([tensor.shape[0], tensor.shape[1], tensor.shape[2], self.session_info['nt'] * len(self.tarray)])
+        aux = reshape_observations( self.super_tensor, self.session_info['nT'], len(self.tarray) )
         return aux
