acb7f518e6fa35588ebe13d59f223f5836055630
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2021-03-24 15:41:52 -0300

    update temp. net. class, converting data containers to xarray

--- GDa/net/util.py
+++ GDa/net/util.py
@@ -1,5 +1,6 @@
 import igraph as ig 
 import numpy  as np
+import xarray as xr
 from   scipy  import stats
 
 def instantiate_graph(A, is_weighted = False):
@@ -9,7 +10,7 @@ def instantiate_graph(A, is_weighted = False):
         g = ig.Graph.Adjacency(A.tolist(), mode=ig.ADJ_UNDIRECTED)
     return g
 
-def compute_thresholds(tensor, q=0.8, relative=False):
+def compute_coherence_thresholds(tensor, q=0.8, relative=False):
     r'''
     Compute the power/coherence thresholds for the data
     > INPUTS:
@@ -17,17 +18,22 @@ def compute_thresholds(tensor, q=0.8, relative=False):
     - q: Quartile value to use as threshold
     - relative: If True compute one threshold for each node/link in each band (defalta False)
     > OUTPUTS:
-    - coh_thr: Threshold values, if realtive is True it will have dimensions [bands, nodes/links] otherwise [bands]
+    - coh_thr: Threshold values, if realtive is True it will have dimensions ["links","bands","trials"] otherwise ["bands","trials"]
     '''
-    n_nodes, n_bands, n_obs = tensor.shape[0], tensor.shape[1], tensor.shape[2]
+    n_nodes, n_bands, n_trials, n_obs = tensor.shape[0], tensor.shape[1], tensor.shape[2], tensor.shape[3]
     if relative:
-        coh_thr = np.zeros([n_bands, n_nodes])
-        for i in range( n_bands ):
-            coh_thr[i] = np.squeeze( stats.mstats.mquantiles(tensor[:,i,:], prob=q, axis=1) )
+        coh_thr = np.zeros([n_nodes, n_bands, n_trials])
+        for t in range( n_trials ):
+            for i in range( n_bands ):
+                coh_thr[:,i,t] = np.squeeze( stats.mstats.mquantiles(tensor[:,i,t,:], prob=q, axis=-1) )
+        coh_thr = xr.DataArray(coh_thr, dims=("links","bands","trials"))
     else:
-        coh_thr = np.zeros( n_bands )
-        for i in range( n_bands ):
-            coh_thr[i] = stats.mstats.mquantiles(tensor[:,i,:].flatten(), prob=q)
+        coh_thr = np.zeros( [n_bands, n_trials] )
+        for t in range( n_trials ):
+            for i in range( n_bands ):
+                coh_thr[i,t] = stats.mstats.mquantiles(tensor[:,i,t,:].flatten(), prob=q)
+        coh_thr = xr.DataArray(coh_thr, dims=("bands","trials"))
+    
     return coh_thr
 
 def check_symmetric(a, rtol=1e-05, atol=1e-08):
--- GDa/temporal_network.py
+++ GDa/temporal_network.py
@@ -2,7 +2,7 @@ import numpy                as     np
 import xarray               as     xr
 import GDa.session          
 from   GDa.misc.reshape     import reshape_trials, reshape_observations
-from   GDa.net.util         import compute_thresholds
+from   GDa.net.util         import compute_coherence_thresholds
 from   scipy                import stats
 import os
 import h5py
@@ -35,6 +35,12 @@ class temporal_network():
 
         # Load session info
         self.info = GDa.session.session(raw_path=data_raw_path, monkey=monkey, date=date, session=session)
+        # Storing recording info
+        self.recording_info = self.info.recording_info
+        # Storing trial info
+        self.trial_info     = self.info.trial_info
+        #
+        del self.info
 
         # Setting up mokey and recording info to load and save files
         self.raw_path = tensor_raw_path
@@ -43,21 +49,17 @@ class temporal_network():
         self.session  = 'session0' + str(session)       
         
         # Load super-tensor
-        self.__load_h5()
-
-        if trim_borders == True:
-            self.tarray       = self.tarray[wt[0]:-wt[1]]
-            self.super_tensor = self.super_tensor[:,:,:,wt[0]:-wt[1]]
-
+        self.__load_h5(trim_borders, wt)
 
         # Threshold the super tensor
         if threshold:
-            self.coh_thr = compute_thresholds(self.super_tensor, q=q, relative=relative)
-            for i in range( len(self.bands) ):
-                if relative: self.super_tensor[:,i,:] = self.super_tensor[:,i,:]>self.coh_thr[i][:,np.newaxis]
-                else: self.super_tensor[:,i,:] = self.super_tensor[:,i,:]>self.coh_thr[i]
+            self.coh_thr      = compute_coherence_thresholds(self.super_tensor.values, q=q, relative=relative)
+            self.super_tensor.values = (self.super_tensor>self.coh_thr).values
+            #for i in range( len(self.bands) ):
+            #    if relative: self.super_tensor[:,i,:] = self.super_tensor[:,i,:]>self.coh_thr[i][:,np.newaxis]
+            #    else: self.super_tensor[:,i,:] = self.super_tensor[:,i,:]>self.coh_thr[i]
 
-    def __load_h5(self,):
+    def __load_h5(self,trim_borders, wt):
         # Path to the super tensor in h5 format 
         h5_super_tensor_path = os.path.join(self.raw_path, self.monkey+'_'+str(self.session)+'_'+self.date+'.h5')
 
@@ -72,27 +74,51 @@ class temporal_network():
         self.freqs        = hf['freqs'][:]
         self.bands        = hf['bands'][:]
 
-        # Concatenate trials in the super tensor
-        self.super_tensor = self.super_tensor.swapaxes(1,2)
-        self.super_tensor = self.reshape_observations()
-
-        # Convert to xarray
-        self.super_tensor = xr.DataArray(self.super_tensor, dims=("links","bands","observation"),
-                                                            coords={"observations": self.tarray.tolist()*len(self.info.trial_info),
-                                                            "trials": self.info.trial_info.index.values } )
-
         # Reading metadata
+        self.session_info = {}
+        self.session_info['nT'] = self.super_tensor.shape[1]
         for k in hf['info'].keys():
             if k == 'nC':
-                self.super_tensor.attrs[k] = int( np.squeeze( np.array(hf['info/'+k]) ) )
+                self.session_info[k] = int( np.squeeze( np.array(hf['info/'+k]) ) )
             else:
-                self.super_tensor.attrs[k] = np.squeeze( np.array(hf['info/'+k]) )
+                self.session_info[k] = np.squeeze( np.array(hf['info/'+k]) )
+
+        if trim_borders == True:
+            self.tarray       = self.tarray[wt[0]:-wt[1]]
+            self.super_tensor = self.super_tensor[:,:,:,wt[0]:-wt[1]]
+
+        # Concatenate trials in the super tensor
+        self.super_tensor = self.super_tensor.swapaxes(1,2)
+        #self.super_tensor = self.reshape_observations()
+
+        # Convert to xarray
+#        self.super_tensor = xr.DataArray(self.super_tensor, dims=("links","bands","observations"),
+#                                                            coords={"observations": self.tarray.tolist()*self.session_info['nT'],
+#                                                            } )
+        self.super_tensor = xr.DataArray(self.super_tensor, dims=("links","bands","trials","time"),
+                                         coords={"trials": self.trial_info.index.values, 
+                                                 "time":   self.tarray} )
+        # Metadata (the same as session_info)
+        self.super_tensor.attrs = self.session_info
 
     def convert_to_adjacency(self,):
-        self.A = np.zeros([self.session_info['nC'], self.session_info['nC'], len(self.bands), self.session_info['nT']*len(self.tarray)]) 
+        self.A = np.zeros([self.session_info['nC'], self.session_info['nC'], 
+                           len(self.bands), self.session_info['nT'], len(self.tarray)]) 
+
         for p in range(self.session_info['pairs'].shape[0]):
-            i, j              = self.session_info['pairs'][p,0], self.session_info['pairs'][p,1]
-            self.A[i,j,:,:]   = self.super_tensor[p,:,:]
+            i, j               = self.session_info['pairs'][p,0], self.session_info['pairs'][p,1]
+            self.A[i,j,:,:,:]  = self.super_tensor[p,:,:,:]
+
+#    def convert_to_adjacency(self,):
+#        self.A = np.zeros([self.session_info['nC'], self.session_info['nC'], len(self.bands), self.session_info['nT']*len(self.tarray)]) 
+#        for p in range(self.session_info['pairs'].shape[0]):
+#            i, j              = self.session_info['pairs'][p,0], self.session_info['pairs'][p,1]
+#            self.A[i,j,:,:]   = self.super_tensor[p,:,:]
+#
+    def reshape_trials(self, ):
+        aux = reshape_trials( self.super_tensor, self.session_info['nT'], len(self.tarray) )
+        return aux
+
 
     def reshape_trials(self, ):
         aux = reshape_trials( self.super_tensor, self.session_info['nT'], len(self.tarray) )
