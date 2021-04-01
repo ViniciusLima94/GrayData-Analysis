d64c6010ba69357d2f19f73408056d2497a76501
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2021-04-01 16:13:15 -0300

    update otebook 5

--- config.py
+++ config.py
@@ -24,16 +24,30 @@ bands = {
         'lucy':  [[0.1,6],[6,14],[14,26],[26,42],[42,80]],
         'ethyl': [[0.1,8],[8,21],[21,33],[33,80]]
         }
-# Range of frequencies to be analyzed
-freqs = np.linspace(0.1, 80, 50) 
-# Delta for downsampling
-delta = 15
-# Number of cycles for the wavelet
-n_cycles = freqs/2
-# Time bandwidth for the multitaper
-time_bandwidth = 8
+
 # Smoothing windows
 win_freq = 1   # Freq
 win_time = 34  # Time
+# Delta for downsampling
+delta = 15
 # Method
-method   = 'morlet'
+method   = 'multitaper'
+
+if method == 'morlet':
+    # Range of frequencies to be analyzed
+    freqs = np.linspace(0.1, 80, 50) 
+    # Number of cycles for the wavelet
+    n_cycles = freqs/2
+    # Time bandwidth for the multitaper
+    time_bandwidth = None
+if method == 'multitaper':
+    # Time bandwidth for the multitaper
+    time_bandwidth = 8
+    # Frequncy centers
+    freqs = np.mean( bands['lucy'], axis = 1)
+    # Bandwidth
+    d_f = np.ceil( (np.array(bands['lucy'])[:,1]-np.array(bands['lucy'])[:,0])/2 )
+    # Time-bandwidth
+    time_bandwidth = 8.0
+    # Number of cycles
+    n_cycles = time_bandwidth * freqs / d_f
