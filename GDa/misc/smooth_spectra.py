b9b46835aa7a55928002f0eed928e884b730334b
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2021-04-13 16:19:08 -0300

    update notebook 3.2

--- GDa/misc/smooth_spectra.py
+++ GDa/misc/smooth_spectra.py
@@ -3,13 +3,13 @@ import numpy as np
 
 def smooth_spectra(spectra, win_time, win_freq, fft=False, axes = 0):
 	if len(spectra.shape) == 2:
-		kernel = np.ones([win_freq, win_time])#/(win_freq*win_time)
+		kernel = np.ones([win_freq, win_time])/(win_freq*win_time)
 	elif len(spectra.shape) == 3:
-		kernel = np.ones([1, win_freq, win_time])#/(win_freq*win_time)
+		kernel = np.ones([1, win_freq, win_time])/(win_freq*win_time)
 
 	#print(kernel.shape)
 
 	if fft == True:
-		return scipy.signal.fftconvolve(spectra, kernel, mode='same', axes= axes)
+		return scipy.signal.fftconvolve(spectra, kernel, mode='same', axes= axes)# + 1j*scipy.signal.fftconvolve(spectra.imag, kernel, mode='same', axes= axes)
 	else:
-		return scipy.signal.convolve2d( spectra, kernel, mode='same', axes = axes) 
+		return scipy.signal.convolve(spectra, kernel, mode='same')# + 1j*scipy.signal.convolve( spectra.imag, kernel, mode='same') 
