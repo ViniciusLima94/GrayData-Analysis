b33724bd8ce4875b72b00af8720a513a3015f071
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2021-05-28 06:55:56 -0300

    up sh

--- burstness_analysis.sh
+++ burstness_analysis.sh
@@ -3,7 +3,7 @@
 #SBATCH -J BST                # Job name
 #SBATCH -o bst.out            # Name of stdout output file (%j expands to %jobID)
 #SBATCH -t 700:00:00          # Run time (hh:mm:ss) - 1.5 hours
-#SBATCH -n 1
+#SBATCH -n 40
 #SBATCH -N 1
 #SBATCH --array=0-25
 
