a289d512b6e81b3fd5ef901cc8aa8e6269235c65
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2021-05-29 06:24:38 -0300

    plot script

---
+++ plot_burstness_analysis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -J BST                # Job name
+#SBATCH -o bst.out            # Name of stdout output file (%j expands to %jobID)
+#SBATCH -t 700:00:00          # Run time (hh:mm:ss) - 1.5 hours
+#SBATCH -n 40
+#SBATCH -N 1
+#SBATCH --array=0-25
+
+
+python3.6 -O plot_burstness_analysis.py $SLURM_ARRAY_TASK_ID
