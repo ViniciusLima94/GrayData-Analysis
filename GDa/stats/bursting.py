29681c3488f812c5b54308803c4e86797c68d586
ViniciusLima94 <vinicius.lima.cordeiro@usp.br>
2021-06-04 19:52:11 +0200

    update stats/bursting

--- GDa/stats/bursting.py
+++ GDa/stats/bursting.py
@@ -81,6 +81,7 @@ def tensor_find_activation_sequences(spike_train, mask, dt=None, drop_edges=Fals
     > OUTPUTS:
     - act_lengths: Array containing the length of activations for each link and trial
     '''
+
     # Checking inputs
     assert isinstance(spike_train, (np.ndarray, xr.DataArray))
     assert isinstance(mask, (dict, np.ndarray, xr.DataArray))
