943ef25c869eaf4f5ae34bcaacd5e2061e31aef9
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2021-06-15 14:39:16 +0200

    update in script

--- save_coherences.sh
+++ save_coherences.sh
@@ -2,11 +2,12 @@
 
 #SBATCH -J COH                # Job name
 #SBATCH -o coh.out            # Name of stdout output file (%j expands to %jobID)
-#SBATCH -t 700:00:00          # Run time (hh:mm:ss) - 1.5 hours
-#SBATCH -n 40
-#SBATCH -N 1
-#SBATCH --array=0-25
+#SBATCH --ntasks=40
+#SBATCH --mem-per-cpu=2000
+#SBATCH --cpus-per-task=1
+#SBATCH --array=0-64
 
-module load python/3.6.8
-python3 -O save_coherences.py $SLURM_ARRAY_TASK_ID
+#module load python/3.6.8
+#python3 -O save_coherences.py $SLURM_ARRAY_TASK_ID
+python -O save_coherences.py $SLURM_ARRAY_TASK_ID
 #python3 -O save_coherences.py
