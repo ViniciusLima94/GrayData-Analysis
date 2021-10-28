a0c2367e7f544e89c4941a81d2ba4a01fe110c39
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2021-10-28 14:48:12 +0200

    update notebooks

--- GDa/signal/surrogates.py
+++ GDa/signal/surrogates.py
@@ -12,14 +12,23 @@ def _is_odd(number):
     return bool(number%2)
 
 def trial_swap_surrogates(x, seed=0, verbose=False):
-    r'''
+    """
     Given the data, randomly swap the trials of the channels.
-    > INPUTS:
-    - x: data array with dimensions ("trials","roi","time").
-    - verbose: If True shows progress information.
-    > OUTPUTS:
-    x_surr: Data with randomized trials.
-    '''
+
+    Parameters
+    ----------
+    x: array_like
+        data array with dimensions ("trials","roi","time").
+
+    seed: int
+        seed used for the trial swapping
+
+    Returns
+    -------
+    x_surr: array_like
+        Data with randomized trials ("trials","roi","time").
+.
+    """
 
     np.random.seed(seed)
 
@@ -45,67 +54,56 @@ def trial_swap_surrogates(x, seed=0, verbose=False):
 
     return x_surr
 
-def phase_rand_surrogates(x, val=1, seed=0, verbose=False, n_jobs=1):
-    r'''
-    PhaseRand_surrogates(x, val) takes time-series array (x) and type of randomization (val)
-    as inputs and provides phase-randomized TS as output.
-    > INPUTS:
-    - x: data array with dimensions ("trials","roi","time").
-    - val: '1' (default) phases are coherently randomized, i.e. to preserve the same sample covariance matrix as the
-                original TS(thus randomizing dFC, but not FC) '0' phases are incoherently randomized (thus randomizing
-                both dFC and FC)
-    - n_jobs: Number of jobs to parallelize over trials
-    > OUTPUTS:
-    x_surr: Surrogated signal.
-    '''
+def phase_rand_surrogates(x, seed=0, verbose=False, n_jobs=1):
+    """
+    PhaseRand_surrogates takes time-series array. Phases are coherently randomized, i.e. 
+    to preserve the same sample covariance matrix as the original 
+    TS (thus randomizing dFC, but not FC).
+
+    Parameters
+    ----------
+    x: array_like
+        data array with dimensions ("trials","roi","time").
+    seed: int | 0
+        seed used for the trial swapping
+    n_jobs: int | 1
+        Number of jobs to parallelize over trials
+
+    Returns
+    -------
+    x_surr: array_like
+        Phase-randomized surrogated signal ("trials","roi","time").
+    """
     
     np.random.seed(seed)
 
     assert isinstance(x, (np.ndarray, xr.DataArray))
-    assert isinstance(val, int)
-
+ 
     # Get number of nodes and time points
     n_trials, n_nodes, n_times = x.shape[0], x.shape[1], x.shape[2]
 
     def _for_trial(trial):
         # Get fft of the signal
-        x_fft = np.fft.fft(x[trial], axis=-1)
-
-        if val==0:
-            phase_rnd = np.zeros((n_nodes,n_times))
-            # In case the number of time points is odd
-            if _is_odd(n_times):
-                ph = 2*pi*np.random.rand(n_nodes,(n_times-1)//2)-pi
-                phase_rnd[:,1:] = np.concatenate((ph,-ph[:,::-1]),axis=-1) 
-            # In case the number of points in even
-            if not _is_odd(n_times):
-                ph = 2*pi*np.random.rand(n_nodes,(n_times-2)//2)-pi
-                phase_rnd[:,1:] = np.concatenate((ph,np.zeros((n_nodes,1)),-ph[:,::-1]),axis=-1)
-            # Randomize the phases of each channel
-            x_fft_rnd = np.zeros_like(x_fft)
-            for m in range(n_nodes):
-                x_fft_rnd[m,:] = np.abs(x_fft[m,:])*np.exp(1j*(np.angle(x_fft[m,:])+phase_rnd[m,:]))
-                x_fft_rnd[m,0] = x_fft[m,0]
-        if val==1:
-            # Construct (conjugate symmetric) array of random phases
-            phase_rnd    = np.zeros(n_times)
-            # Define first phase
-            phase_rnd[0] = 0
-            # In case the number of time points is odd
-            if _is_odd(n_times):
-                ph = 2*pi*np.random.rand((n_times-1)//2)-pi
-                phase_rnd[1:] = np.concatenate((ph,-ph))
-            # In case the number of points in even
-            if not _is_odd(n_times):
-                ph = 2*pi*np.random.rand((n_times-2)//2)-pi
-                phase_rnd[1:] = np.concatenate((ph,np.zeros(1),-ph[::-1]))
-            # Randomize the phases of each channel
-            x_fft_rnd = np.zeros_like(x_fft)
-            for m in range(n_nodes):
-                x_fft_rnd[m,:] = np.abs(x_fft[m,:])*np.exp(1j*(np.angle(x_fft[m,:])+phase_rnd))
-                x_fft_rnd[m,0] = x_fft[m,0]
+        x_fft = np.fft.fft(x[trial,...], axis=-1)
+        # Construct (conjugate symmetric) array of random phases
+        phase_rnd    = np.zeros(n_times)
+        # Define first phase
+        phase_rnd[0] = 0
+        # In case the number of time points is odd
+        if _is_odd(n_times):
+            ph = 2*pi*np.random.rand((n_times-1)//2)-pi
+            phase_rnd[1:] = np.concatenate((ph,-np.flip(ph,-1)))
+        # In case the number of points in even
+        if not _is_odd(n_times):
+            ph = 2*pi*np.random.rand((n_times-2)//2)-pi
+            phase_rnd[1:] = np.concatenate((ph,np.zeros(1),-np.flip(ph,-1)))
+        # Randomize the phases of each channel
+        x_fft_rnd = np.zeros_like(x_fft)
+        for m in range(n_nodes):
+            x_fft_rnd[m,:] = np.abs(x_fft[m,:])*np.exp(1j*(np.angle(x_fft[m,:])+phase_rnd))
+            x_fft_rnd[m,0] = x_fft[m,0]
         # Transform back to time domain
-        x_rnd = np.fft.ifft(x_fft_rnd)
+        x_rnd = np.fft.ifft(x_fft_rnd,axis=-1)
         return x_rnd.real
 
     # Parallelize on trials
