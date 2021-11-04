1ac41f27385a2cc52375eb70bd7dc58f5e8d902f
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2021-11-04 17:55:13 +0100

    update temporal network class and burst stats script

--- save_burst_stats.sh
+++ save_burst_stats.sh
@@ -5,6 +5,6 @@
 #SBATCH --ntasks=40
 #SBATCH --mem-per-cpu=2000
 #SBATCH --cpus-per-task=1
-#SBATCH --array=0-3
+#SBATCH --array=0-1
 
-python -O save_burst_stats.py "morlet" $SLURM_ARRAY_TASK_ID
+python -O save_burst_stats.py "coh" "morlet" $SLURM_ARRAY_TASK_ID
