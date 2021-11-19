5a6a7a9eb6c9e567ab26e6c5b81c377acc10be48
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2021-11-19 15:31:32 +0100

    small fixes

--- plot_average_coherence.py
+++ plot_average_coherence.py
@@ -1,29 +1,31 @@
-import numpy as np 
+import numpy as np
 import os
 import sys
-from   GDa.temporal_network import temporal_network
+from GDa.temporal_network import temporal_network
 import matplotlib.pyplot as plt
 
-idx = int( sys.argv[-1] )
+idx = int(sys.argv[-1])
 
 ###########################################################################
 # Get sessions names
 ###########################################################################
-sessions = np.array( os.listdir("Results/lucy/") )
+sessions = np.array(os.listdir("Results/lucy/"))
 
 ###########################################################################
-# Plotting 
+# Plotting
 ###########################################################################
 s = sessions[idx]
 
-net = temporal_network(coh_file='coh_k_0.3_morlet.nc', coh_sig_file=None,
+net = temporal_network(coh_file='coh_k_0.3_morlet.nc',
+                       coh_sig_file='coh_k_0.3_morlet_surr.nc',
                        date=s, trial_type=[1], behavioral_response=[1])
 
 # Average over the same rois
-out = net.super_tensor.groupby("roi").mean("roi").interp({"freqs":np.linspace(3,75,30)}, "quadratic")
+out = net.super_tensor.groupby("roi").mean("roi")
+# .interp(# {"freqs": np.linspace(3, 75, 30)}, "quadratic")
 
 # Average over time
-out.mean("times").mean("trials").plot(x="freqs", hue="roi", color="b", lw=1);
+out.mean("times").mean("trials").plot(x="freqs", hue="roi", color="b", lw=1)
 plt.xlim(out.freqs[0], out.freqs[-1])
 plt.legend([])
 plt.title(f"{s}")
