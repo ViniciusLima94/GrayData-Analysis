d102892b911acddef74928076a6f2ca02ec55fb9
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2021-11-24 18:18:36 +0100

    update on net.null_models and mi scripts

--- GDa/net/null_models.py
+++ GDa/net/null_models.py
@@ -1,52 +1,65 @@
 import random
-import numpy  as np 
+import numpy as np
+import numba as nb
 import xarray as xr
-import igraph as ig
-from   tqdm   import tqdm
-from   .util  import instantiate_graph, _check_inputs, _unwrap_inputs
+from tqdm import tqdm
+from util import instantiate_graph, _check_inputs, _unwrap_inputs
 
 _DEFAULT_TYPE = np.float32
+
 
 def shuffle_frames(A, seed=0):
 
     # Checking inputs
     _check_inputs(A, 3)
     # Get values in case it is an xarray
-    if isinstance(A, xr.DataArray): A = A.values
+    if isinstance(A, xr.DataArray):
+        A = A.values
 
     #  Number of observations
     nt = A.shape[-1]
     #  Set randomization seed
     np.random.seed(seed)
     #  Observation indexes
-    idx = np.arange(nt, dtype = int)
+    idx = np.arange(nt, dtype=int)
     np.random.shuffle(idx)
-    A_null = ( A + np.transpose( A,  (1,0,2) ) ).copy()
-    A_null = A_null[:,:,idx]
+    A_null = (A + np.transpose(A,  (1, 0, 2))).copy()
+    A_null = A_null[:, :, idx]
 
     return A_null.astype(_DEFAULT_TYPE)
 
-def randomize_edges(A, n_rewires = 100, seed=0, verbose=False):
-    r'''
+
+def randomize_edges(A, n_rewires=100, seed=0, verbose=False):
+    """
     Randomize an adjacency matrix while maintaining its nodes' degrees
     and undirectionality.
-    > INPUTS:
-    - A: Multiplex adjacency matrix with shape (roi,roi,trials,time).
-    - n_rewires: Number of rewires to be performed in each adjacency matrix.
-    - seed: Seed for random edge selection.
-    - verbose: Wheater to print the progress or not.
-    > OUTPUTS:
-    - A_null: The randomized multiplex adjacency matrix with shape (roi,roi,trials,time).
-    '''
+
+    Parameters:
+    ----------
+
+    A: array_like
+        Multiplex adjacency matrix with shape (roi,roi,trials,time).
+    n_rewires: int | 100
+        Number of rewires to be performed in each adjacency matrix.
+    seed: int | 0
+        Seed for random edge selection.
+
+    Returns:
+    -------
+    A_null: array_like
+        The randomized multiplex adjacency matrix
+        with shape (roi,roi,trials,time).
+    """
     # Set random seed (for python's default random and numpy)
-    random.seed(seed); np.random.seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
 
     # Check inputs
     _check_inputs(A, 4)
     # Get values in case it is an xarray
-    A, roi, trials, time = _unwrap_inputs(A,concat_trials=True)
+    A, roi, trials, time = _unwrap_inputs(A, concat_trials=True)
     #  Number of channels
-    nC = A.shape[0]
+    # nC = A.shape[0]
     #  Number of observations
     nt = A.shape[-1]
 
@@ -54,17 +67,119 @@ def randomize_edges(A, n_rewires = 100, seed=0, verbose=False):
 
     itr = range(nt)
     for t in (tqdm(itr) if verbose else itr):
-        g   = instantiate_graph(A[...,t], is_weighted=False)
-        G   = g.copy()
+        g = instantiate_graph(A[..., t], is_weighted=False)
+        G = g.copy()
         G.rewire(n=n_rewires)
-        A_null[...,t] = np.array(list(G.get_adjacency()))
+        A_null[..., t] = np.array(list(G.get_adjacency()))
 
     # Unstack trials and time
-    A_null = A_null.reshape( (len(roi),len(roi),len(trials),len(time)) )
+    A_null = A_null.reshape((len(roi), len(roi), len(trials), len(time)))
     # Convert to xarray
-    A_null = xr.DataArray(A_null.astype(_DEFAULT_TYPE), dims=("sources","targets","trials","times"),
-                                  coords={"sources": roi,
-                                          "targets": roi,
-                                          "times": time,
-                                          "trials": trials} )
+    A_null = xr.DataArray(A_null.astype(_DEFAULT_TYPE),
+                          dims=("sources", "targets", "trials", "times"),
+                          coords={"sources": roi,
+                                  "targets": roi,
+                                  "times": time,
+                                  "trials": trials})
     return A_null
+
+
+def dragon_head_randomizer(A, k=2, seed=0, verbose=False):
+    """
+    Randomize an adjacency matrix by shuffling the edges
+    for a subgraph formed by k-nodes. The nodes are selected
+    in a way that try to minimize the difference of the weighth
+    values of the edges beling to the subgraph.
+
+    This is done by first sorting the edges according to weights,
+    then the first k strongest edges are selected and have are
+    "rewired". At the end the null adjancency matrix if built
+    by projecting the rewired edges to the (n_nodes, n_nodes)
+    matrix.
+
+    Parameters:
+    ----------
+
+    A: array_like
+        Multiplex adjacency matrix with shape (roi,roi,trials,time).
+    k: int | 2
+        Size of the k-plets to use should be at least 2.
+    seed: int | 0
+        Seed for random edge selection.
+
+    Returns:
+    -------
+    A_null: array_like
+        The randomized multiplex adjacency matrix
+        with shape (roi,roi,trials,time).
+    """
+    np.random.seed(seed)
+
+    # Get number of nodes, trials and edges
+    n_nodes, n_trials, n_times = A.shape[0], A.shape[2], A.shape[3]
+    n_edges = int(n_nodes*(n_nodes-1)/2)
+
+    # The number of k-plets should be proportional to
+    # k, the begginiing of the sorted edges is removed
+    # in order to achieve #k-plets%k = 0.
+    rem = int((np.arange(n_edges) % k)[-1]+1)
+
+    # Total number of observations
+    nt = n_trials * n_times
+
+    # Containner for the null matrix
+    Anull = A.copy()
+    Anull = Anull.reshape((n_nodes, n_nodes, nt))
+
+    itr = range(nt)
+    for t in (tqdm(itr) if verbose else itr):
+        # Get edges
+        B = Anull[..., t][np.triu_indices(n_nodes, 1)]
+
+        # Sort edges and weights
+        sort = np.argsort(B)
+        wsort = np.sort(B)
+
+        # Reshape in kplets form
+        sorted_nodes = sort[rem:].reshape((-1, k))
+        sorted_wei = wsort[rem:].reshape((-1, k))
+        _sorted_nodes, _sorted_wei = _shuffle_kplets(sorted_nodes, sorted_wei)
+
+        # Bring back to matrix form
+        out = _sorted_wei.flatten()[np.argsort(sort[rem:])]
+        print(f"{out.shape}")
+        print(f"{wsort[:rem].shape}")
+        out = np.hstack((wsort[:rem], out))
+        # Modify Anull inplace
+        # Anull[np.triu_indices(n_nodes), t] = out
+        Anull[..., t][np.triu_indices(n_nodes, 1)] = out
+        # N = np.zeros((n_nodes, n_nodes))
+        # N[np.triu_indices(n_nodes, 1)] = out
+    Anull = Anull.reshape((n_nodes, n_nodes, n_trials, n_times))
+    return Anull+Anull.transpose((1, 0, 2, 3))
+
+
+@nb.jit(nopython=True)
+def _shuffle_kplets(sorted_nodes, sorted_wei):
+    """
+    Receives an array with the nodes sorted by weights
+    and the weights organized in "k-plets", i.e., an
+    array with n rows and k columns.
+
+    Parameters:
+    ----------
+    sorted_nodes: array_like
+        Index of the nodes sorted by weights
+        with shape (n,k).
+    sorted_wei: array_like
+        Weights sorted with shape (n,k).
+    """
+    # Store the shuffled version of sorted_nodes
+    _sorted_nodes = sorted_nodes.copy()
+    # Store the shuffled version of sorted_wei
+    _sorted_wei = sorted_wei.copy()
+    for idx in range(sorted_nodes.shape[0]):
+        np.random.shuffle(_sorted_nodes[idx, :])
+        ###############################
+        np.random.shuffle(_sorted_wei[idx, :])
+    return _sorted_nodes, _sorted_wei
--- GDa/net/util.py
+++ GDa/net/util.py
@@ -1,14 +1,13 @@
-import igraph as ig 
-import numpy  as np
-import numba  as nb
+import igraph as ig
+import numpy as np
+import numba as nb
 import xarray as xr
-import scipy
-from   scipy        import stats
-from   tqdm         import tqdm
-from   frites.utils import parallel_func
+from scipy import stats
+from frites.utils import parallel_func
+
 
 @nb.njit
-def _is_binary(matrix): 
+def _is_binary(matrix):
     """
     Check if a matrix is binary or weighted.
 
@@ -24,6 +23,7 @@ def _is_binary(matrix):
             break
     return is_binary
 
+
 def _convert_to_affiliation_vector(n_nodes, partitions):
     """
     Convert partitions in leidenalg format to array.
@@ -32,7 +32,8 @@ def _convert_to_affiliation_vector(n_nodes, partitions):
     n_nodes: int
         The number of nodes.
     partitions: ModularityVertexPartition
-        Parition objects of type "leidenalg.VertexPartition.ModularityVertexPartition"
+        Parition objects of type
+        "leidenalg.VertexPartition.ModularityVertexPartition"
     Returns
     -------
     av: array_like
@@ -41,25 +42,28 @@ def _convert_to_affiliation_vector(n_nodes, partitions):
     # Extact list of partitions
     #  partitions = partitions.values[0]
     # Number of time points
-    n_times    = len(partitions)
+    n_times = len(partitions)
     # Affiliation vector
-    av         = np.zeros((n_nodes,n_times))
+    av = np.zeros((n_nodes, n_times))
     for t in range(n_times):
-        for comm_i, comm in enumerate( partitions[t] ):
-            av[comm,t] = comm_i
+        for comm_i, comm in enumerate(partitions[t]):
+            av[comm, t] = comm_i
     return av
 
+
 @nb.jit(nopython=True)
-def MODquality(A,av,gamma=1):
+def MODquality(A, av, gamma=1):
     """
-    Given an affiliation vector compute the modularity of the graph given by A (adapted from brainconn).
+    Given an affiliation vector compute the modularity of the graph
+    given by A (adapted from brainconn).
 
     Parameters
     ----------
     A: array_like
         Adjacency matrix.
     av: array_like
-        Affiliation vector of size (n_nodes) containing the label of the community of each node.
+        Affiliation vector of size (n_nodes) containing the label of the
+        community of each node.
     gamma: float
         Value of gamma to use.
 
@@ -70,18 +74,18 @@ def MODquality(A,av,gamma=1):
     # Number of nodes
     n_nodes = A.shape[0]
     # Degrees
-    d       = A.sum(0)
+    d = A.sum(0)
     # Number of edges
     n_edges = np.sum(d)
     # Initial modularity matrix
-    B       = A - gamma * np.outer(d, d) / n_edges
+    B = A - gamma * np.outer(d, d) / n_edges
     #  B       = B[av][:, av]
     # Tile affiliation vector
-    s       = av.repeat(n_nodes).reshape((-1, n_nodes))#np.tile(av, (n_nodes, 1))
+    s = av.repeat(n_nodes).reshape((-1, n_nodes))
     return np.sum(np.logical_not(s - s.T) * B / n_edges)
 
-#@nb.jit(nopython=True)
-def CPMquality(A,av,gamma=1):
+
+def CPMquality(A, av, gamma=1):
     """
     Constant Potts Model (CPM) quality function.
 
@@ -90,7 +94,8 @@ def CPMquality(A,av,gamma=1):
     A: array_like
         Adjacency matrix.
     av: array_like
-        Affiliation vector of size (n_nodes) containing the label of the community of each node.
+        Affiliation vector of size (n_nodes) containing the label of the
+        community of each node.
     gamma: float
         Value of gamma to use.
 
@@ -99,22 +104,19 @@ def CPMquality(A,av,gamma=1):
     H: float
         The quality given by the CPM model.
     """
-    av    = av.astype(int)
+    av = av.astype(int)
     # Total number of communities
     n_comm = int(np.max(av))+1
     # Quality index
-    H      = 0
+    H = 0
     for c in range(n_comm):
         # Find indexes of channels in the commucnit C
-        idx = av==c
+        idx = av == c
         # Number of nodes in community C
         n_c = np.sum(idx)
-        H   = H + np.sum(A[np.ix_(av[idx],av[idx])])-gamma*n_c*(n_c-1)/2
-        a =np.sum(A[np.ix_(av[idx],av[idx])])
-        b = n_c*(n_c-1)/2
-        #  print(f'{a=}')
-        #  print(f'{b=}')
+        H = H + np.sum(A[np.ix_(av[idx], av[idx])])-gamma*n_c*(n_c-1)/2
     return H
+
 
 def _check_inputs(array, dims):
     """
@@ -129,7 +131,8 @@ def _check_inputs(array, dims):
     """
     assert isinstance(dims, int)
     assert isinstance(array, (np.ndarray, xr.DataArray))
-    assert len(array.shape)==dims, f"The adjacency tensor should be {dims}D."
+    assert len(array.shape) == dims, f"The adjacency tensor should be {dims}D."
+
 
 def _unwrap_inputs(array, concat_trials=False):
     """
@@ -146,37 +149,41 @@ def _unwrap_inputs(array, concat_trials=False):
     -------
     array values concatenated or not and the values for each of its dimensions.
     """
-    if isinstance(array, xr.DataArray): 
+    if isinstance(array, xr.DataArray):
         # Concatenate trials and time axis
         try:
-            roi    = array.roi_1.values
+            roi = array.roi_1.values
             trials = array.trials.values
-            time   = array.time.values
+            time = array.time.values
         except:
-            roi    = np.arange(0, array.shape[0])
+            roi = np.arange(0, array.shape[0])
             trials = np.arange(0, array.shape[2])
-            time   = np.arange(0, array.shape[3])
-        if concat_trials: array = array.stack(observations=("trials","times"))
+            time = np.arange(0, array.shape[3])
+        if concat_trials:
+            array = array.stack(observations=("trials", "times"))
         array = array.values
     else:
-        roi    = np.arange(0, array.shape[0])
+        roi = np.arange(0, array.shape[0])
         trials = np.arange(0, array.shape[2])
-        time   = np.arange(0, array.shape[3])
-        if concat_trials: array = array.reshape( (len(roi),len(roi),len(trials)*len(time)) )
+        time = np.arange(0, array.shape[3])
+        if concat_trials:
+            array = array.reshape((len(roi), len(roi), len(trials)*len(time)))
     return array, roi, trials, time
+
 
 def _reshape_list(array, shapes, dtype):
     assert isinstance(shapes, tuple)
     assert isinstance(array,  list)
-    idx       = 0
+    idx = 0
     container = np.zeros(shapes, dtype=dtype)
     for i in range(shapes[0]):
         for j in range(shapes[1]):
-            container[i,j]=array[idx]
+            container[i, j] = array[idx]
             idx += 1
     return container
 
-def convert_to_adjacency(tensor,sources,targets, dtype=np.float32):
+
+def convert_to_adjacency(tensor, sources, targets, dtype=np.float32):
     """
     Convert the tensor with the edge time-series to a matrix representations.
     
@@ -194,23 +201,25 @@ def convert_to_adjacency(tensor,sources,targets, dtype=np.float32):
     The adjacency matrix (roi,roi,freqs,trials,times).
     """
 
-    assert tensor.ndim==4
-    assert tensor.shape[0]==len(sources)==len(targets)
+    assert tensor.ndim == 4
+    assert tensor.shape[0] == len(sources) == len(targets)
 
     # Number of pairs
-    n_pairs,n_bands,n_trials,n_times = tensor.shape[:]
+    n_pairs, n_bands, n_trials, n_times = tensor.shape[:]
     # Number of channels
-    n_channels = int(np.roots([1,-1,-2*n_pairs])[0])
+    n_channels = int(np.roots([1, -1, -2*n_pairs])[0])
 
     # Adjacency tensor
-    A = np.zeros([n_channels, n_channels, n_bands, n_trials, n_times],dtype=dtype)
+    A = np.zeros([n_channels, n_channels, n_bands,
+                 n_trials, n_times], dtype=dtype)
 
     for p in range(n_pairs):
-        i, j        = sources[p], targets[p]
-        A[i,j,...]  = A[j,i,...] = tensor[p,...]
+        i, j = sources[p], targets[p]
+        A[i, j, ...] = A[j, i, ...] = tensor[p, ...]
     return A
 
-def instantiate_graph(A, is_weighted = False):
+
+def instantiate_graph(A, is_weighted=False):
     """
     Convert a numpy array adjacency matrix into a igraph object
     
@@ -226,88 +235,67 @@ def instantiate_graph(A, is_weighted = False):
     The adjacency matrix as an igraph object.
     """
     if is_weighted:
-        g = ig.Graph.Weighted_Adjacency(A.tolist(), attr="weight", loops=False, mode=ig.ADJ_UNDIRECTED)
+        g = ig.Graph.Weighted_Adjacency(
+            A.tolist(), attr="weight", loops=False, mode=ig.ADJ_UNDIRECTED)
     else:
         g = ig.Graph.Adjacency(A.tolist(), mode=ig.ADJ_UNDIRECTED)
     return g
 
-def compute_coherence_thresholds(tensor, q=0.8, relative=False, verbose=False, n_jobs=1):
+
+def compute_coherence_thresholds(tensor, q=0.8, relative=False, verbose=False,
+                                 n_jobs=1):
     """
     Compute the power/coherence thresholds for the data
 
     Parameters
     ----------
     tensor: array_like
-        Data with dimensions (nodes/links,bands,observations) or (nodes/links,bands,trials,time)
+        Data with dimensions (nodes/links,bands,observations)
+        or (nodes/links,bands,trials,time)
     q: array_like | 0.8
         Quantile value to use as threshold
     relative: bool | False
-        If True compute one threshold for each node/link in each band (defalta False)
+        If True compute one threshold for each node/link
+        in each band (defalta False)
 
     Returns
     -------
     coh_thr: array_like
-        Threshold values, if realtive is True it will have dimensions ("links","bands","trials") otherwise ("bands","trials") (if tensor shape is 3 there is no "trials" dimension)
+        Threshold values, if realtive is True it will have
+        dimensions ("links","bands","trials") otherwise ("bands","trials")
+        (if tensor shape is 3 there is no "trials" dimension)
     """
-    n_nodes, n_bands, n_obs = tensor.shape[0], tensor.shape[1], tensor.shape[2]
+    n_nodes, n_bands = tensor.shape[0], tensor.shape[1]
     # To compute in parallel for each band
+
     def _for_band(b):
         if relative:
-            out = np.squeeze( stats.mstats.mquantiles(tensor[:,b,:], prob=q, axis=-1) )
+            out = np.squeeze(stats.mstats.mquantiles(
+                tensor[:, b, :], prob=q, axis=-1))
         else:
-            out = stats.mstats.mquantiles(tensor[:,b,:].flatten(), prob=q)
+            out = stats.mstats.mquantiles(tensor[:, b, :].flatten(), prob=q)
         return out
 
     # Create containers
-    if relative: 
-        coh_thr = xr.DataArray(np.zeros([n_nodes, n_bands]), dims=("roi","freqs"))
+    if relative:
+        coh_thr = xr.DataArray(
+            np.zeros([n_nodes, n_bands]), dims=("roi", "freqs"))
     else:
         coh_thr = xr.DataArray(np.zeros(n_bands), dims=("freqs"))
-    
-    itr = range(n_bands) # Iterator
+
+    # itr = range(n_bands)  # Iterator
     # define the function to compute in parallel
     parallel, p_fun = parallel_func(
         _for_band, n_jobs=n_jobs, verbose=verbose,
         total=n_bands)
     # Compute the single trial coherence
-    out = np.squeeze( parallel(p_fun(t) for t in range(n_bands)) )
+    out = np.squeeze(parallel(p_fun(t) for t in range(n_bands)))
     coh_thr.values = np.stack(out, -1)
     return coh_thr
 
-    #  if len(tensor.shape)==4: 
-    #      n_nodes, n_bands, n_trials, n_obs = tensor.shape[0], tensor.shape[1], tensor.shape[2], tensor.shape[3]
-    #      if relative:
-    #          coh_thr = np.zeros([n_nodes, n_bands, n_trials])
-    #          itr = range(n_trials) # Iterator
-    #          for t in (tqdm(itr) if verbose else itr):
-    #              for i in range( n_bands ):
-    #                  coh_thr[:,i,t] = np.squeeze( stats.mstats.mquantiles(tensor[:,i,t,:], prob=q, axis=-1) )
-    #          coh_thr = xr.DataArray(coh_thr, dims=("roi","freqs","trials"))
-    #      else:
-    #          coh_thr = np.zeros( [n_bands, n_trials] )
-    #          itr = range(n_trials) # Iterator
-    #          for t in (tqdm(itr) if verbose else itr):
-    #              for i in range( n_bands ):
-    #                  coh_thr[i,t] = stats.mstats.mquantiles(tensor[:,i,t,:].flatten(), prob=q)
-    #          coh_thr = xr.DataArray(coh_thr, dims=("freqs","trials"))
-    #  if len(tensor.shape)==3: 
-    #      n_nodes, n_bands, n_obs = tensor.shape[0], tensor.shape[1], tensor.shape[2]
-    #  if relative:
-    #      coh_thr = np.zeros([n_nodes, n_bands])
-    #      itr = range(n_bands) # Iterator
-    #      for i in (tqdm(itr) if verbose else itr):
-    #          coh_thr[:,i] = np.squeeze( stats.mstats.mquantiles(tensor[:,i,:], prob=q, axis=-1) )
-    #      coh_thr = xr.DataArray(coh_thr, dims=("roi","freqs"))
-    #  else:
-    #      coh_thr = np.zeros( n_bands )
-    #      itr = range(n_bands) # Iterator
-    #      for i in (tqdm(itr) if verbose else itr):
-    #          coh_thr[i] = stats.mstats.mquantiles(tensor[:,i,:].flatten(), prob=q)
-    #      coh_thr = xr.DataArray(coh_thr, dims=("freqs"))
-    #  return coh_thr
 
 def check_symmetric(a, rtol=1e-05, atol=1e-08):
     """
     Check if the matrix a is symmetric
     """
-    return numpy.allclose(a, a.T, rtol=rtol, atol=atol)
+    return np.allclose(a, a.T, rtol=rtol, atol=atol)
