45052c080374f5516ef1750f571ad832c430fa5c
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2021-11-26 17:06:05 +0100

    update mi scripts

--- mi_coh_analysis.py
+++ mi_coh_analysis.py
@@ -3,47 +3,30 @@ import numpy as np
 from GDa.temporal_network import temporal_network
 from frites.dataset import DatasetEphy
 from frites.workflow import WfMi
-# import matplotlib.pyplot as plt
-import argparse
-
-###############################################################################
-# Argument parsing
-###############################################################################
-
-parser = argparse.ArgumentParser()
-parser.add_argument("SIDX",   help="index of the session to run",
-                    type=int)
-
-args = parser.parse_args()
-
-# Index of the session to be load
-idx = args.SIDX
 
 _ROOT = os.path.expanduser('~/storage1/projects/GrayData-Analysis')
 # Get session number
 sessions = np.loadtxt("GrayLab/lucy/sessions.txt", dtype=str)
-s_id = sessions[idx]
 
 ###############################################################################
-# Loading coherence dFC
+# Iterate over all sessions and concatenate coherece
 ###############################################################################
 
-# Instantiating temporal network
-net = temporal_network(coh_file='coh_k_0.3_morlet.nc',
-                       coh_sig_file='coh_k_0.3_morlet_surr.nc',
-                       date=s_id, trial_type=[1],
-                       behavioral_response=[1])
+coh = []
+stim = []
+for s_id in sessions[:2]:
+    # Instantiating temporal network
+    net = temporal_network(coh_file='coh_k_0.3_morlet.nc',
+                           # coh_sig_file='coh_k_0.3_morlet_surr.nc',
+                           date=s_id, trial_type=[1],
+                           behavioral_response=[1])
 
-net.super_tensor \
-    = net.super_tensor.transpose("trials", "roi", "freqs", "times")
+    net.super_tensor \
+        = net.super_tensor.transpose("trials", "roi", "freqs", "times")
 
-###############################################################################
-# Iterate over all sessions and concatenate power
-###############################################################################
-
-coh = [net.super_tensor.isel(roi=[r])
-       for r in range(len(net.super_tensor['roi']))]
-stim = [net.super_tensor.attrs["stim"].astype(int)] \
+    coh += [net.super_tensor.isel(roi=[r])
+            for r in range(len(net.super_tensor['roi']))]
+    stim += [net.super_tensor.attrs["stim"].astype(int)] \
         * len(net.super_tensor['roi'])
 
 
@@ -52,7 +35,7 @@ stim = [net.super_tensor.attrs["stim"].astype(int)] \
 ###############################################################################
 
 # Convert to DatasetEphy
-dt = DatasetEphy([coh], y=stim, nb_min_suj=2,
+dt = DatasetEphy(coh, y=stim, nb_min_suj=2,
                  times="times", roi="roi")
 
 mi_type = 'cd'
@@ -65,39 +48,8 @@ cluster_th = None  # {float, None, 'tfce'}
 
 mi, pvalues = wf.fit(dt, mcp="cluster", cluster_th=cluster_th, **kw)
 
-# Setting roi names for mi and pvalues
-# mi = mi.assign_coords({"roi": net.super_tensor.roi.values})
-# pvalues = pvalues.assign_coords({"roi": net.super_tensor.roi.values})
+path_mi = os.path.join(_ROOT, "Results/lucy/mi_power_rfx/mi_coh.nc")
+path_pv = os.path.join(_ROOT, "Results/lucy/mi_power_rfx/pval_coh.nc")
 
-###############################################################################
-# Saving data
-###############################################################################
-
-path_st = os.path.join(_ROOT, f"Results/lucy/{s_id}/session01")
-path_mi = os.path.join(path_st, "coh_mi_values.nc")
-path_pval = os.path.join(path_st, "coh_p_values.nc")
-
-# Not storing for storage issues
-# mi.to_netcdf(path_mi)
-# pvalues.to_netcdf(path_pval)
-
-# Thresholded p-values
-# p_values_thr = pvalues <= 0.05
-
-# p_values_thr.to_netcdf(path_pval)
-
-###############################################################################
-# Plotting values with p<=0.05
-###############################################################################
-
-# out = mi * p_values_thr
-
-# plt.figure(figsize=(20, 6))
-# for i in range(10):
-    # plt.subplot(2, 5, i+1)
-    # plt.imshow(out.isel(freqs=i), aspect="auto", origin="lower",
-               # cmap="turbo")
-    # plt.colorbar()
-# plt.tight_layout()
-
-# plt.savefig(f"figures/mi_coh/mi_coh_{s_id}.png", dpi=150)
+mi.to_netcdf(path_mi)
+pvalues.to_netcdf(path_pv)
--- mi_power_analysis.py
+++ mi_power_analysis.py
@@ -3,7 +3,6 @@ import xarray as xr
 import numpy as np
 from frites.dataset import DatasetEphy
 from frites.workflow import WfMi
-# import matplotlib.pyplot as plt
 
 _ROOT = os.path.expanduser('~/storage1/projects/GrayData-Analysis')
 # Get session number
@@ -48,47 +47,3 @@ path_pv = os.path.join(_ROOT, "Results/lucy/mi_power_rfx/pval_power.nc")
 
 mi.to_netcdf(path_mi)
 pvalues.to_netcdf(path_pv)
-
-# # Setting roi names for mi and pvalues
-# mi = mi.assign_coords({"roi": data.roi.values})
-# pvalues = pvalues.assign_coords({"roi": data.roi.values})
-
-###############################################################################
-# Saving data
-###############################################################################
-
-# path_st = os.path.join(_ROOT, f"Results/lucy/{s_id}/session01")
-# path_mi = os.path.join(path_st, "power_mi_values.nc")
-# path_pval = os.path.join(path_st, "power_p_values.nc")
-
-# Not storing for storage issues
-# mi.to_netcdf(path_mi)
-# pvalues.to_netcdf(path_pval)
-
-# p_values_thr = pvalues <= 0.05
-# p_values_thr.to_netcdf(path_pval)
-
-###############################################################################
-# Plotting values with p<=0.05
-###############################################################################
-# out = mi * p_values_thr
-
-# plt.figure(figsize=(20, 6))
-# for i in range(10):
-# plt.subplot(2, 5, i+1)
-# idx = out.isel(freqs=i).sum("times") > 0
-# out.isel(freqs=i, roi=idx).plot(x="times", hue="roi")
-# plt.legend([])
-# plt.tight_layout()
-
-# plt.figure(figsize=(20, 6))
-# for i in range(10):
-# plt.subplot(2, 5, i+1)
-# plt.imshow(out.isel(freqs=i).T,
-# cmap="turbo", aspect="auto", origin="lower",
-# extent=[out.times.values[0], out.times.values[-1], 0, 57])
-# plt.yticks(range(57), out.roi.values)
-# plt.title(f"f={out.freqs.values[i]}")
-
-
-# plt.savefig(f"figures/mi_power/mi_power_{s_id}.png", dpi=150)
