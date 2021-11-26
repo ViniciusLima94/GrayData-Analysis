c57fcc9a70c4c85c14a96a51e6902c181cec91ab
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2021-11-26 12:52:46 +0100

    script to perform rfx analysis on MI

--- mi_power_analysis.py
+++ mi_power_analysis.py
@@ -3,7 +3,7 @@ import xarray as xr
 import numpy as np
 from frites.dataset import DatasetEphy
 from frites.workflow import WfMi
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 _ROOT = os.path.expanduser('~/storage1/projects/GrayData-Analysis')
 # Get session number
@@ -15,19 +15,22 @@ sessions = np.loadtxt("GrayLab/lucy/sessions.txt", dtype=str)
 
 sxx = []
 stim = []
-for s_id in sessions[:10]:
+roi = []
+for s_id in sessions[:60]:
     path_pow = os.path.join(_ROOT, f"Results/lucy/{s_id}/session01/power.nc")
     out = xr.load_dataarray(path_pow)
     sxx += [out.isel(roi=[r]) for r in range(len(out['roi']))]
     stim += [out.attrs["stim"].astype(int)]*len(out['roi'])
+    roi += [out['roi'].data.tolist()]
 
+roi = [item for sublist in roi for item in sublist]
 
 ###############################################################################
 # MI Workflow
 ###############################################################################
 
 # Convert to DatasetEphy
-dt = DatasetEphy(sxx, y=stim,
+dt = DatasetEphy(sxx, y=stim, nb_min_suj=2,
                  times="times", roi="roi")
 
 mi_type = 'cd'
@@ -39,6 +42,12 @@ kw = dict(n_jobs=20, n_perm=200)
 cluster_th = None  # {float, None, 'tfce'}
 
 mi, pvalues = wf.fit(dt, mcp="cluster", cluster_th=cluster_th, **kw)
+
+path_mi = os.path.join(_ROOT, "Results/lucy/mi_power_rfx/mi_power.nc")
+path_pv = os.path.join(_ROOT, "Results/lucy/mi_power_rfx/pval_power.nc")
+
+mi.to_netcdf(path_mi)
+pvalues.to_netcdf(path_pv)
 
 # # Setting roi names for mi and pvalues
 # mi = mi.assign_coords({"roi": data.roi.values})
@@ -66,9 +75,20 @@ mi, pvalues = wf.fit(dt, mcp="cluster", cluster_th=cluster_th, **kw)
 
 # plt.figure(figsize=(20, 6))
 # for i in range(10):
-    # plt.subplot(2, 5, i+1)
-    # idx = out.isel(freqs=i).sum("times") > 0
-    # out.isel(freqs=i, roi=idx).plot(x="times", hue="roi")
+# plt.subplot(2, 5, i+1)
+# idx = out.isel(freqs=i).sum("times") > 0
+# out.isel(freqs=i, roi=idx).plot(x="times", hue="roi")
+# plt.legend([])
 # plt.tight_layout()
+
+# plt.figure(figsize=(20, 6))
+# for i in range(10):
+# plt.subplot(2, 5, i+1)
+# plt.imshow(out.isel(freqs=i).T,
+# cmap="turbo", aspect="auto", origin="lower",
+# extent=[out.times.values[0], out.times.values[-1], 0, 57])
+# plt.yticks(range(57), out.roi.values)
+# plt.title(f"f={out.freqs.values[i]}")
+
 
 # plt.savefig(f"figures/mi_power/mi_power_{s_id}.png", dpi=150)
