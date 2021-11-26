370ab676b715095e64ab7cdfe90897a201502cad
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2021-11-26 18:31:48 +0100

    save power for other trials types and beh. responses

--- save_power.sh
+++ save_power.sh
@@ -5,6 +5,7 @@
 #SBATCH --ntasks=40
 #SBATCH --mem-per-cpu=2000
 #SBATCH --cpus-per-task=1
-#SBATCH --array=33-64
+#SBATCH --array=0-64
 
-python -O save_power.py $SLURM_ARRAY_TASK_ID
+python -O save_power.py $SLURM_ARRAY_TASK_ID 1 1
+python -O save_power.py $SLURM_ARRAY_TASK_ID 1 0
