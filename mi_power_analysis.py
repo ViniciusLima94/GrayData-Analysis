0a9e7dd4b2969be34c87717752e7740bc0b14942
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2021-11-26 21:40:57 +0100

    mi for tt and br different

--- mi_power_analysis.py
+++ mi_power_analysis.py
@@ -3,6 +3,28 @@ import xarray as xr
 import numpy as np
 from frites.dataset import DatasetEphy
 from frites.workflow import WfMi
+from tqdm import tqdm
+import argparse
+
+###############################################################################
+# Argument parsing
+###############################################################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument("TT",   help="type of the trial",
+                    type=int)
+parser.add_argument("BR",   help="behavioral response",
+                    type=int)
+
+args = parser.parse_args()
+
+# Index of the session to be load
+tt = args.TT
+br = args.BR
+
+###############################################################################
+# Get root path and session names
+###############################################################################
 
 _ROOT = os.path.expanduser('~/storage1/projects/GrayData-Analysis')
 # Get session number
@@ -15,8 +37,10 @@ sessions = np.loadtxt("GrayLab/lucy/sessions.txt", dtype=str)
 sxx = []
 stim = []
 roi = []
-for s_id in sessions[:60]:
-    path_pow = os.path.join(_ROOT, f"Results/lucy/{s_id}/session01/power.nc")
+for s_id in tqdm(sessions[:60]):
+    path_pow = \
+        os.path.join(_ROOT,
+                     f"Results/lucy/{s_id}/session01/power_tt_{tt}_br_{br}.nc")
     out = xr.load_dataarray(path_pow)
     sxx += [out.isel(roi=[r]) for r in range(len(out['roi']))]
     stim += [out.attrs["stim"].astype(int)]*len(out['roi'])
@@ -42,8 +66,10 @@ cluster_th = None  # {float, None, 'tfce'}
 
 mi, pvalues = wf.fit(dt, mcp="cluster", cluster_th=cluster_th, **kw)
 
-path_mi = os.path.join(_ROOT, "Results/lucy/mi_power_rfx/mi_power.nc")
-path_pv = os.path.join(_ROOT, "Results/lucy/mi_power_rfx/pval_power.nc")
+path_mi = os.path.join(_ROOT,
+                       f"Results/lucy/mi_pow_rfx/mi_pow_tt_{tt}_br_{br}.nc")
+path_pv = os.path.join(_ROOT,
+                       f"Results/lucy/mi_pow_rfx/pval_pow_{tt}_br_{br}.nc")
 
 mi.to_netcdf(path_mi)
 pvalues.to_netcdf(path_pv)
