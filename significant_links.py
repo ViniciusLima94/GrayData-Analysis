210a17dca752feec60f022a3a4f6e34cb2c4b874
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2022-01-13 17:56:51 +0100

    update script for compute siginificant links

--- significant_links.py
+++ significant_links.py
@@ -29,7 +29,7 @@ _SEED = 8179273
 evt_dt = return_evt_dt(at)
 
 #######################################################################
-# Compute coherence for original and surrogate data 
+# Compute coherence for original and surrogate data
 #######################################################################
 #  Instantiating session
 ses = session(raw_path="GrayLab/",
@@ -76,6 +76,7 @@ then we cannot reject the null hypothesis in favor of the alternative.
 that our observation is not so unlikely to have occurred by chance.
 """
 
+
 def significance_test(verbose=False, n_jobs=1):
     """
     Method to compute the ks- and t-test for each frequency band
@@ -108,18 +109,19 @@ def significance_test(verbose=False, n_jobs=1):
     p_values = parallel(p_fun(band) for band in range(coh.shape[1]))
     return np.asarray(p_values).T
 
+
 # Compute p-values
 p_values = significance_test(verbose=True, n_jobs=10)
 # Significance level
 alpha = 0.05
 # Convert to xarray
-p_values = xr.DataArray(p_values, dims = ("roi", "p", "freqs"), 
-                        coords=dict(roi=coh.roi.data, 
+p_values = xr.DataArray(p_values, dims=("roi", "p", "freqs"),
+                        coords=dict(roi=coh.roi.data,
                                     freqs=coh.freqs.data,
                                     p=["ks", "t"]))
 
 #######################################################################
-# Compute number of sig. links between each pair of regions 
+# Compute number of sig. links between each pair of regions
 #######################################################################
 
 # Number of regions between pairs of channels
@@ -129,7 +131,32 @@ roi_s, roi_t = _extract_roi(p_sig.roi.data, '-')
 unique_rois = np.unique(np.concatenate((roi_s, roi_t)))
 mapping = dict(zip(unique_rois, range(len(unique_rois))))
 n_rois = len(unique_rois)
+n_pairs = len(roi_s)
 
 sources, targets = [], []
 sources += [mapping[s] for s in roi_s]
 targets += [mapping[t] for t in roi_t]
+
+p_mat = xr.DataArray(np.zeros((n_rois, n_rois, 2, p_sig.sizes["freqs"])),
+                     dims=("sources", "targets", "p", "freqs"),
+                     coords=dict(sources=unique_rois,
+                                 targets=unique_rois,
+                                 p=["ks", "t"],
+                                 freqs=p_sig.freqs.data)
+                     )
+
+for p, roi in enumerate(p_sig.roi.data):
+    x, y = roi.split("-")
+    s, t = mapping[x], mapping[y]
+    p_mat[s, t] = p_mat[t, s] = p_sig[p]
+
+
+#######################################################################
+# Saving to data frame
+#######################################################################
+
+df = p_sig.to_dataframe(name='n_edges').reset_index()
+x_s, x_t = _extract_roi(df['roi'].values, '-')
+df['sources'] = x_s
+df['targets'] = x_t
+df.to_csv(f'Results/lucy/significance_analysis/n_edges_{sessions[idx]}.csv')
