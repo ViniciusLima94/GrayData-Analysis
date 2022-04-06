9b2e3e111e328aeed7dbdf6ccddc6fb0bacd4381
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2022-04-06 17:33:25 +0200

    up

--- meta_conn.sh
+++ meta_conn.sh
@@ -7,8 +7,8 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --array=0-61
 
-#python -O meta_conn.py $SLURM_ARRAY_TASK_ID "coh"
+python -O meta_conn.py $SLURM_ARRAY_TASK_ID "coh" 1
 #python -O meta_conn.py $SLURM_ARRAY_TASK_ID "plv"
 #python -O meta_conn.py $SLURM_ARRAY_TASK_ID "pec"
 
-python -O trimmer_strengths.py "pec" $SLURM_ARRAY_TASK_ID
+#python -O trimmer_strengths.py "coh" $SLURM_ARRAY_TASK_ID 1
