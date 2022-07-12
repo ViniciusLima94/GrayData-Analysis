2fdd2eee6ed68dbf71f51cb21324fb7256012c01
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2022-07-12 09:15:20 +0200

    up

--- beta_gamma_corr.py
+++ beta_gamma_corr.py
@@ -11,6 +11,7 @@ import seaborn as sns
 import xarray as xr
 from frites.conn.conn_tf import _tf_decomp
 from mne.time_frequency import psd_array_multitaper
+from mne.filter import filter_data
 from scipy.signal import find_peaks
 from tqdm import tqdm
 
@@ -25,10 +26,17 @@ sid = sessions[idx]
 def flatten(xss):
     return [x for xs in xss for x in xs]
 
-#### Setting spectral analysis parameters
 
+# Define path to sabe figures
+figs_path = f"figures/betagamma/{sid}"
 
-# Defining parameters
+if not os.path.exists(figs_path):
+        os.makedirs(figs_path)
+
+##############################################################################
+# Setting spectral analysis parameters
+##############################################################################
+
 decim = 20  # Downsampling factor
 mode = "multitaper"  # Wheter to use Morlet or Multitaper
 n_freqs = 100  # How many frequencies to use
@@ -44,9 +52,9 @@ bands = {
     "gamma": [43, 80],
 }
 
-#### Loanding data
-
-
+##############################################################################
+# Loanding data
+##############################################################################
 def load_session_data(sid):
 
     # Instantiate class
@@ -77,10 +85,11 @@ def load_session_data(sid):
 
 data = load_session_data(sid)
 
-#### Computing trial averaged static spectra
+##############################################################################
+# Computing trial averaged static spectra
+##############################################################################
 
 sfreq = data.fsample
-
 
 power_static, freqs = psd_array_multitaper(
     data, sfreq, fmin=0, fmax=80, bandwidth=1, n_jobs=20
@@ -96,7 +105,9 @@ power_static = xr.DataArray(
 power_static = power_static.mean("trials")
 power_static = power_static / power_static.max("freqs")
 
-#### Distribution of peak promininces
+##############################################################################
+# Distribution of peak promininces
+##############################################################################
 
 
 def detect_peak_frequencies(power=None, prominence=0.01, verbose=False):
@@ -134,7 +145,9 @@ has_peak = xr.DataArray(
     has_peak, dims=("roi", "bands"), coords=(data.roi, list(bands.keys()))
 )
 
-#### Time-frequency multitaper spectra
+##############################################################################
+# Time-frequency multitaper spectra
+##############################################################################
 
 w = _tf_decomp(
     data,
@@ -163,9 +176,11 @@ gamma = slice(60, 70)
 # Power
 power = (w * np.conj(w)).real
 
+# Power time series in hgamma and beta band
 power_beta = power.sel(freqs=beta).mean("freqs")
 power_gamma = power.sel(freqs=gamma).mean("freqs")
 
+# Select channels with peaks in both beta and gamma bands 
 indexes = np.logical_and(has_peak[:, 3], has_peak[:, 4])
 power_beta = power_beta.isel(roi=indexes)
 power_gamma = power_gamma.isel(roi=indexes)
@@ -176,8 +191,11 @@ phi = np.angle(scipy.signal.hilbert(power_beta)) - np.angle(
 )
 phi = xr.DataArray(phi, dims=power_beta.dims, coords=power_beta.coords)
 
+# z-scored and max norm time-series
 z_beta = (power_beta - power_beta.mean("times")) / power_beta.std("times")
 z_gamma = (power_gamma - power_gamma.mean("times")) / power_gamma.std("times")
+max_beta = power_beta / power_beta.max(("times", "trials"))
+max_gamma = power_gamma / power_gamma.max(("times", "trials"))
 
 cc = (z_beta * z_gamma).mean("times")
 
@@ -185,37 +203,48 @@ trials_shuffle = z_gamma.trials.data
 
 CC = []
 
+### pseudorandom
 for i in tqdm(range(1000)):
 
+    trials_shuffle = z_gamma.trials.data.copy()
+
+    np.random.seed(i + 1000)
+
     np.random.shuffle(trials_shuffle)
 
-    x1 = z_beta.sel(trials=trials_shuffle)
+    x1 = z_beta.sel(trials=trials_shuffle).values
 
-    np.random.shuffle(trials_shuffle)
+    trials_shuffle = z_gamma.trials.data.copy()
 
-    x2 = z_gamma.sel(trials=trials_shuffle)
+    x2 = z_gamma.sel(trials=np.roll(trials_shuffle, 30)).values
 
-    cc_surr = (x1 * x2).mean("times")
+    cc_surr = (x1 * x2).mean(-1)
 
     CC += [cc_surr]
 
-
-df = pd.concat(
-    [
-        z_beta.to_dataframe("power_beta").reset_index(),
-        z_gamma.to_dataframe("power_gamma").reset_index(),
-        phi.to_dataframe("phi").reset_index(),
-    ],
-    axis=1,
+CC = xr.DataArray(
+    np.vstack(CC),
+    dims=("trials", "roi"),
+    coords={"roi": z_gamma.roi},
 )
 
-df = df.loc[:, ~df.columns.duplicated()].copy()
+# df = pd.concat(
+    # [
+        # z_beta.to_dataframe("power_beta").reset_index(),
+        # z_gamma.to_dataframe("power_gamma").reset_index(),
+        # phi.to_dataframe("phi").reset_index(),
+    # ],
+    # axis=1,
+# )
+
+# df = df.loc[:, ~df.columns.duplicated()].copy()
 
 cc_time = z_beta * z_gamma
 
 has_beta_gamma = has_peak.sel(bands=['beta_2', 'gamma']).sum('bands') == 2
 
 roi_idx = has_beta_gamma.roi[has_beta_gamma].data
+
 try:
     roi_idx = np.random.choice(roi_idx, 3, replace=False)
 except ValueError:
@@ -308,31 +337,35 @@ plt.xlabel("Frequency [Hz]")
 
 ##############################################################################
 plt.sca(ax6)
-thr = xr.concat(CC, dim="surr").quantile(0.95, "surr")
-thr = thr.to_dataframe("thr").reset_index()
-
-mean = thr.groupby(["roi"]).mean("thr")
-std = thr.groupby(["roi"])["thr"].std()
-
 df = cc.to_dataframe(name="cc").reset_index()
 df = df.sort_values("cc", ascending=False)
 
-sns.boxplot(x=df["roi"], y=df["cc"], color="lightgray",
-            medianprops=dict(color="k", label='median'))
+ax6 = sns.boxplot(
+    x=df["roi"],
+    y=df["cc"],
+    color="cyan",
+    medianprops=dict(color="b", label="median"),
+)
 
 rois = [t.get_text() for t in ax6.get_xticklabels()]
 
-mean = mean.reindex(index=rois).thr.values
-std = std.reindex(index=rois).reset_index().thr.values
+CC = CC.sel(roi=rois)
+
+mean = CC.median("trials")
+std1 = CC.quantile(0.05, "trials")
+std2 = CC.quantile(0.95, "trials")
+
+plt.plot(range(cc.sizes["roi"]), mean, "k-")
 
 plt.fill_between(
     range(cc.sizes["roi"]),
-    mean - std,
-    mean + std,
-    alpha=0.5,
-    color="red",
+    std1,
+    std2,
+    alpha=0.2,
+    color="gray",
 )
-plt.xticks(rotation=30, fontsize=8)
+
+plt.xticks(rotation=45)
 plt.xlabel("")
 ax6.spines["right"].set_visible(False)
 ax6.spines["top"].set_visible(False)
@@ -356,5 +389,142 @@ plt.ylabel("Phase")
 ax7.spines["top"].set_visible(False)
 ax7.spines["right"].set_visible(False)
 
-plt.savefig(f"figures/betagamma/beta_gamma_{sid}")
+plt.savefig(os.path.join(figs_path, f"beta_gamma_{sid}.png"))
 plt.close()
+
+
+##############################################################################
+# Triggered averaged gamma cycles plots
+##############################################################################
+
+data = data.sel(roi=indexes)
+
+
+def triggered_avg(
+    data=None,
+    high_pass=None,
+    win_size=None,
+    height=None,
+    find_troughs=None,
+    verbose=False,
+):
+
+    n_trials, n_roi, n_times = data.shape
+
+    # High-pass filtered data
+    data_hp = filter_data(data, data.fsample,
+                          l_freq=high_pass, h_freq=None, verbose=verbose)
+
+    # Converts back to DataArray
+    data_hp = xr.DataArray(data_hp, dims=data.dims, coords=data.coords)
+
+    win_size = int(win_size * data.fsample)
+
+    data_hp = data_hp.data.swapaxes(0, 1).reshape(n_roi, n_trials * n_times)
+    if find_troughs:
+        data_hp = -data_hp
+
+    def _for_roi(i):
+        peaks, _ = find_peaks(data_hp[i], height)
+        snipets = np.zeros((len(peaks), 2 * win_size))
+        for pidx, idx in enumerate(peaks):
+            temp = data_hp[i, (idx - win_size): (idx + win_size)]
+            if len(temp) == 2 * win_size:
+                snipets[pidx, :] = temp
+
+        return snipets
+
+    snipets = [_for_roi(i) for i in range(n_roi)]
+
+    return snipets
+
+
+def cycle_triggered_avg(
+    data=None,
+    band_b=None,
+    band_g=None,
+    win_size=None,
+    height=None,
+    find_troughs=None,
+    verbose=False,
+):
+
+    n_trials, n_roi, n_times = data.shape
+
+    # Beta component
+    data_beta = filter_data(
+        data, data.fsample, band_b[0], band_b[1], verbose=verbose)
+    # Gamma co,ponent
+    data_gamma = filter_data(
+        data, data.fsample, band_g[0], band_g[1], verbose=verbose)
+
+    # Converts back to DataArray
+    data_beta = xr.DataArray(data_beta, dims=data.dims, coords=data.coords)
+    data_gamma = xr.DataArray(data_gamma, dims=data.dims, coords=data.coords)
+
+    win_size = int(win_size * data.fsample)
+
+    data_beta = data_beta.data.swapaxes(
+        0, 1).reshape(n_roi, n_trials * n_times)
+    if find_troughs:
+        data_beta = -data_beta
+
+    data_gamma = data_gamma.data.swapaxes(
+        0, 1).reshape(n_roi, n_trials * n_times)
+
+    def _for_roi(i):
+        peaks, _ = find_peaks(data_beta[i], height)
+        snipets = np.zeros((len(peaks), 2 * win_size))
+        for pidx, idx in enumerate(peaks):
+            temp = data_gamma[i, (idx - win_size): (idx + win_size)]
+            if len(temp) == 2 * win_size:
+                snipets[pidx, :] = temp
+
+        return snipets
+
+    snipets = [_for_roi(i) for i in range(n_roi)]
+
+    return snipets
+
+
+# High-passed signal
+snipets_hp = triggered_avg(
+    data=data,
+    high_pass=15,
+    win_size=0.1,
+    height=0.00015,
+    find_troughs=True
+)
+
+# Gamma averaged cycles
+snipets = cycle_triggered_avg(
+    data=data,
+    band_b=(28, 42),
+    band_g=(58, 72),
+    win_size=0.015,
+    height=0.00015,
+    find_troughs=True,
+)
+
+n_channel = len(snipets)
+
+plt.figure(figsize=(15, 6))
+for r in range(n_channel):
+    plt.subplot(2, n_channel, r + 1)
+    for i in range(len(snipets_hp[r])):
+        plt.plot(np.arange(-100, 100), snipets_hp[r][i], "b", lw=0.05)
+    plt.plot(np.arange(-100, 100), snipets_hp[r].mean(0), "k", lw=4)
+    plt.title(f"{data.roi.data[r]}")
+
+
+for r in range(n_channel):
+    plt.subplot(2, n_channel, n_channel + r + 1)
+    for i in range(len(snipets[r])):
+        plt.plot(np.arange(-15, 15), snipets[r][i], "b", lw=0.05)
+    plt.plot(np.arange(-15, 15), snipets[r].mean(0), "k", lw=4)
+    plt.title(f"{data.roi.data[r]}")
+plt.tight_layout()
+
+plt.savefig(os.path.join(figs_path, f"snipets_{sid}.png"))
+plt.close()
+
