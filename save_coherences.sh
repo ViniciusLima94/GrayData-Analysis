6f8d3cc6c61e2668cfbd194c62ae1f7aae8c9479
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2022-09-23 14:51:07 +0200

    up

--- save_coherences.sh
+++ save_coherences.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=40
 #SBATCH --mem-per-cpu=2000
 #SBATCH --cpus-per-task=1
-#SBATCH --array=0-61
+#SBATCH --array=0-25
 
-python -O save_coherences.py "coh" "cue" $SLURM_ARRAY_TASK_ID 1 0
+python -O save_coherences.py "coh" "cue" $SLURM_ARRAY_TASK_ID 1 0 "ethyl"
 
