4fc6c15fdf44b9df21c7c1afd957b10f176cf112
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2022-09-26 15:49:57 +0200

    up

--- meta_conn.sh
+++ meta_conn.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=40
 #SBATCH --mem-per-cpu=2000
 #SBATCH --cpus-per-task=1
-#SBATCH --array=0-61
+#SBATCH --array=0-25
 
 # Original
                        # SIDX           METRIC SURR THR MONKEY
