0b8c7ffe9e16f5cc46548b6b632d1adefe7fb727
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2022-10-07 17:03:04 +0200

    ùp

--- compute_mean_coherences.py
+++ compute_mean_coherences.py
@@ -8,6 +8,8 @@ from GDa.util import average_stages
 from GDa.temporal_network import temporal_network
 
 parser = argparse.ArgumentParser()
+parser.add_argument("METRIC", help="which metric to use",
+                    type=str)
 parser.add_argument("MONKEY", help="which monkey to use",
                     type=str)
 parser.add_argument("ALIGNED", help="wheter power was align to cue or match",
@@ -17,6 +19,7 @@ parser.add_argument("DELAY", help="which type of delay split to use",
 
 args = parser.parse_args()
 
+metric = args.METRIC
 monkey = args.MONKEY
 at = args.ALIGNED
 ds = args.DELAY
@@ -26,14 +29,19 @@ early_cue, early_delay = return_delay_split(monkey=monkey, delay_type=ds)
 
 coh_file = 'coh_at_cue.nc'
 coh_sig_file = 'thr_coh_at_cue_surr.nc'
+tt = br = [1]
+if metric == "pec":
+    coh_file = "pec_tt_1_br_1_at_cue.nc"
+    coh_sig_file = None
+    tt = br = None
 
 data = []
 for s_id in tqdm(sessions):
     net = temporal_network(coh_file=coh_file, early_delay=early_delay,
                            early_cue=early_cue, align_to=at,
                            coh_sig_file=coh_sig_file, wt=None,
-                           date=s_id, trial_type=[1], monkey=monkey,
-                           behavioral_response=[1])
+                           date=s_id, trial_type=tt, monkey=monkey,
+                           behavioral_response=br)
     # Average if needed
     out = average_stages(net.super_tensor, 1, early_cue=early_cue,
                          early_delay=early_delay)
@@ -54,6 +62,6 @@ data = data.groupby("roi").mean("roi", skipna=True)
 data = data.sel(roi=urois)
 
 save_path = os.path.expanduser(
-    f"~/funcog/gda/Results/{monkey}/mean_coherences/mean_coh_at_{at}_ds_{ds}.nc")
+    f"~/funcog/gda/Results/{monkey}/mean_coherences/mean_{metric}_at_{at}_ds_{ds}.nc")
 
 data.to_netcdf(save_path)
