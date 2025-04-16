81e13713dbe4e573177724a1caa0b9307508717b
vinicius <vinicius.lima.cordeiro@gmail.com>
2025-04-16 17:19:32 +0200

    up

--- save_power.py
+++ save_power.py
@@ -214,7 +214,7 @@ else:
 #    )[..., ::decim]
 
 sxx = hilbert_decomposition(
-    data, data.attrs["fsample"], "time", "roi", bands["lucy"], 1, True
+    data, data.attrs["fsample"], "time", "roi", bands["lucy"], 15, True
 )
 
 # sm_times = int(np.round(0.1 * data.attrs["fsample"]  / decim))
